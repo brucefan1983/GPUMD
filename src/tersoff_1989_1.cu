58ea49eb3d61e4d6290e8a2778bff4a1d2bb60c2
Zheyong Fan <zheyongfan@163.com>
2018-03-10 16:47:52 +0200

    optimize the tersoff_1989_1 potential
    The new version is 1.6 times as fast as the old version.

--- src/tersoff_1989_1.cu
+++ src/tersoff_1989_1.cu
@@ -181,59 +181,28 @@ static __global__ void find_force_tersoff_step1
 }
 
 
- 
 
-/*----------------------------------------------------------------------------80
-    Calculate forces, potential energy, and virial stress
-------------------------------------------------------------------------------*/
-template <int cal_p, int cal_j, int cal_q>
+
+// second step: calculate the partial forces dU_i/dr_ij
+template <int cal_p>
 static __global__ void find_force_tersoff_step2
 (
     int number_of_particles, int pbc_x, int pbc_y, int pbc_z,
-    Tersoff ters0, 
-    int *g_neighbor_number, int *g_neighbor_list,
+    Tersoff ters0, int *g_neighbor_number, int *g_neighbor_list,
 #ifdef USE_LDG
     const real* __restrict__ g_b, 
     const real* __restrict__ g_bp,
     const real* __restrict__ g_x, 
     const real* __restrict__ g_y, 
     const real* __restrict__ g_z, 
-    const real* __restrict__ g_vx, 
-    const real* __restrict__ g_vy, 
-    const real* __restrict__ g_vz,
     const real* __restrict__ g_box_length,
 #else
-    real* g_b, real* g_bp, real* g_x, real* g_y, real* g_z, 
-    real* g_vx, real* g_vy, real* g_vz, real* g_box_length,
+    real* g_b, real* g_bp, real* g_x, real* g_y, real* g_z, real* g_box_length,
 #endif
-    real *g_fx, real *g_fy, real *g_fz,
-    real *g_sx, real *g_sy, real *g_sz, real *g_potential, 
-    real *g_h, int *g_label, int *g_fv_index, real *g_fv 
+    real *g_potential, real *g_f12x, real *g_f12y, real *g_f12z 
 )
 {
     int n1 = blockIdx.x * blockDim.x + threadIdx.x;
-    __shared__ real s_fx[BLOCK_SIZE_FORCE];
-    __shared__ real s_fy[BLOCK_SIZE_FORCE];
-    __shared__ real s_fz[BLOCK_SIZE_FORCE];
-
-    // if cal_p, then s1~s4 = px, py, pz, U; if cal_j, then s1~s5 = j1~j5
-    __shared__ real s1[BLOCK_SIZE_FORCE];
-    __shared__ real s2[BLOCK_SIZE_FORCE];
-    __shared__ real s3[BLOCK_SIZE_FORCE];
-    __shared__ real s4[BLOCK_SIZE_FORCE];
-    __shared__ real s5[BLOCK_SIZE_FORCE];
-
-
-    s_fx[threadIdx.x] = ZERO; 
-    s_fy[threadIdx.x] = ZERO; 
-    s_fz[threadIdx.x] = ZERO;  
-
-    s1[threadIdx.x] = ZERO; 
-    s2[threadIdx.x] = ZERO; 
-    s3[threadIdx.x] = ZERO;
-    s4[threadIdx.x] = ZERO;
-    s5[threadIdx.x] = ZERO;
-
     if (n1 < number_of_particles)
     {
         int neighbor_number = g_neighbor_number[n1];
@@ -241,14 +210,13 @@ static __global__ void find_force_tersoff_step2
         real x1 = LDG(g_x, n1); 
         real y1 = LDG(g_y, n1); 
         real z1 = LDG(g_z, n1);
-        real vx1 = LDG(g_vx, n1); 
-        real vy1 = LDG(g_vy, n1); 
-        real vz1 = LDG(g_vz, n1);
+        real potential_energy = ZERO;
 
         for (int i1 = 0; i1 < neighbor_number; ++i1)
-        {   
-            int n2 = g_neighbor_list[n1 + number_of_particles * i1];
-            int neighbor_number_2 = g_neighbor_number[n2];
+        { 
+
+            int index = i1 * number_of_particles + n1;   
+            int n2 = g_neighbor_list[index];
 
             real x12  = LDG(g_x, n2) - x1;
             real y12  = LDG(g_y, n2) - y1;
@@ -259,48 +227,31 @@ static __global__ void find_force_tersoff_step2
                 LDG(g_box_length, 1), LDG(g_box_length, 2)
             );
             real d12 = sqrt(x12 * x12 + y12 * y12 + z12 * z12);
+            real d12inv = ONE / d12;
             real fc12, fcp12, fa12, fap12, fr12, frp12;
 
             find_fc_and_fcp(ters0, d12, fc12, fcp12);
             find_fa_and_fap(ters0, d12, fa12, fap12);
             find_fr_and_frp(ters0, d12, fr12, frp12);
-
-            real f12x = ZERO; real f12y = ZERO; real f12z = ZERO;
-            real f21x = ZERO; real f21y = ZERO; real f21z = ZERO;
-         
+   
             // accumulate_force_12 
-            real b12 = LDG(g_b, i1 * number_of_particles + n1);    
-            real factor3 = (fcp12*(fr12-b12*fa12)+fc12*(frp12-b12*fap12))/d12;   
-            f12x += x12 * factor3 * HALF; 
-            f12y += y12 * factor3 * HALF;
-            f12z += z12 * factor3 * HALF;
+            real b12 = LDG(g_b, index);    
+            real factor3=(fcp12*(fr12-b12*fa12)+fc12*(frp12-b12*fap12))*d12inv;   
+            real f12x = x12 * factor3 * HALF; 
+            real f12y = y12 * factor3 * HALF;
+            real f12z = z12 * factor3 * HALF;
 
             if (cal_p) // accumulate potential energy
             {
-                s4[threadIdx.x] += fc12 * (fr12 - b12 * fa12) * HALF;
+                potential_energy += fc12 * (fr12 - b12 * fa12) * HALF;
             }
-
-            // accumulate_force_21
-            int offset = 0;
-            for (int k = 0; k < neighbor_number_2; ++k)
-            {
-                if (n1 == g_neighbor_list[n2 + number_of_particles * k]) 
-                { 
-                    offset = k; break; 
-                }
-            }
-            // b12 here actually means b21
-            b12 = LDG(g_b, offset * number_of_particles + n2);
-            factor3 = (fcp12*(fr12-b12*fa12)+fc12*(frp12-b12*fap12))/d12;   
-            f21x -= x12 * factor3 * HALF; 
-            f21y -= y12 * factor3 * HALF;
-            f21z -= z12 * factor3 * HALF;      
 
             // accumulate_force_123
-            real bp12 = LDG(g_bp, i1 * number_of_particles + n1);
+            real bp12 = LDG(g_bp, index);
             for (int i2 = 0; i2 < neighbor_number; ++i2)
-            {       
-                int n3 = g_neighbor_list[n1 + number_of_particles * i2];   
+            {
+                int index_2 = n1 + number_of_particles * i2;        
+                int n3 = g_neighbor_list[index_2];   
                 if (n3 == n2) { continue; } 
                 real x13 = LDG(g_x, n3) - x1;
                 real y13 = LDG(g_y, n3) - y1;
@@ -314,56 +265,134 @@ static __global__ void find_force_tersoff_step2
                 real fc13, fa13;
                 find_fc(ters0, d13, fc13);
                 find_fa(ters0, d13, fa13); 
-                real bp13 = LDG(g_bp, i2 * number_of_particles + n1);
-                real cos123 = (x12 * x13 + y12 * y13 + z12 * z13) / (d12 * d13);
-                real g123, gp123;
-                find_g_and_gp(ters0, cos123, g123, gp123);
-                real cos_x = x13 / (d12 * d13) - x12 * cos123 / (d12 * d12);
-                real cos_y = y13 / (d12 * d13) - y12 * cos123 / (d12 * d12);
-                real cos_z = z13 / (d12 * d13) - z12 * cos123 / (d12 * d12);
-                real temp123a=(-bp12*fc12*fa12*fc13-bp13*fc13*fa13*fc12)*gp123;
-                real temp123b= - bp13 * fc13 * fa13 * fcp12 * g123 / d12;
-                f12x += (x12 * temp123b + temp123a * cos_x)*HALF; 
-                f12y += (y12 * temp123b + temp123a * cos_y)*HALF;
-                f12z += (z12 * temp123b + temp123a * cos_z)*HALF;
-            }
 
-            // accumulate_force_213 (bp12 here actually means bp21)
-            bp12 = LDG(g_bp, offset * number_of_particles + n2); 
-            for (int i2 = 0; i2 < neighbor_number_2; ++i2)
+                real bp13 = LDG(g_bp, index_2);
+                real one_over_d12d13 = ONE / (d12 * d13);   
+                real cos123 = (x12*x13 + y12*y13 + z12*z13)*one_over_d12d13;
+                real cos123_over_d12d12 = cos123*d12inv*d12inv; 
+                real g123, gp123; 
+                find_g_and_gp(ters0, cos123, g123, gp123);
+
+                real temp123a=(-bp12*fc12*fa12*fc13-bp13*fc13*fa13*fc12)*gp123;
+                real temp123b= - bp13 * fc13 * fa13 * fcp12 * g123 * d12inv;
+                real cos_d = x13 * one_over_d12d13 - x12 * cos123_over_d12d12;
+                f12x += (x12 * temp123b + temp123a * cos_d)*HALF; 
+                cos_d = y13 * one_over_d12d13 - y12 * cos123_over_d12d12;
+                f12y += (y12 * temp123b + temp123a * cos_d)*HALF;
+                cos_d = z13 * one_over_d12d13 - z12 * cos123_over_d12d12;
+                f12z += (z12 * temp123b + temp123a * cos_d)*HALF;
+            }
+            g_f12x[index] = f12x;
+            g_f12y[index] = f12y;
+            g_f12z[index] = f12z;
+        }
+        if (cal_p) // save potential
+        {
+            g_potential[n1] = potential_energy;
+        }
+    }
+}   
+
+
+            
+/*----------------------------------------------------------------------------80
+    Calculate forces, potential energy, and virial stress
+------------------------------------------------------------------------------*/
+template <int cal_p, int cal_j, int cal_q>
+static __global__ void find_force_tersoff_step3
+(
+    int number_of_particles, int pbc_x, int pbc_y, int pbc_z,
+    Tersoff ters0, 
+    int *g_neighbor_number, int *g_neighbor_list,
+#ifdef USE_LDG
+    const real* __restrict__ g_f12x, 
+    const real* __restrict__ g_f12y,
+    const real* __restrict__ g_f12z,
+    const real* __restrict__ g_x, 
+    const real* __restrict__ g_y, 
+    const real* __restrict__ g_z, 
+    const real* __restrict__ g_vx, 
+    const real* __restrict__ g_vy, 
+    const real* __restrict__ g_vz,
+    const real* __restrict__ g_box_length,
+#else
+    real* g_f12x, real* g_f12y, real* g_f12z, real* g_x, real* g_y, real* g_z, 
+    real* g_vx, real* g_vy, real* g_vz, real* g_box_length,
+#endif
+    real *g_fx, real *g_fy, real *g_fz,
+    real *g_sx, real *g_sy, real *g_sz,
+    real *g_h, int *g_label, int *g_fv_index, real *g_fv 
+)
+{
+    int n1 = blockIdx.x * blockDim.x + threadIdx.x;
+    real s_fx = ZERO;
+    real s_fy = ZERO;
+    real s_fz = ZERO;
+
+    // if cal_p, then s1~s4 = px, py, pz, U; if cal_j, then s1~s5 = j1~j5
+    __shared__ real s1[BLOCK_SIZE_FORCE];
+    __shared__ real s2[BLOCK_SIZE_FORCE];
+    __shared__ real s3[BLOCK_SIZE_FORCE];
+    __shared__ real s4[BLOCK_SIZE_FORCE];
+    __shared__ real s5[BLOCK_SIZE_FORCE];
+    s1[threadIdx.x] = ZERO; 
+    s2[threadIdx.x] = ZERO; 
+    s3[threadIdx.x] = ZERO;
+    s4[threadIdx.x] = ZERO;
+    s5[threadIdx.x] = ZERO;
+
+    if (n1 < number_of_particles)
+    {
+        int neighbor_number = g_neighbor_number[n1];
+
+        real x1 = LDG(g_x, n1); 
+        real y1 = LDG(g_y, n1); 
+        real z1 = LDG(g_z, n1);
+
+        real vx1, vy1, vz1;
+        if (cal_j || cal_q)
+        {
+            vx1 = LDG(g_vx, n1);
+            vy1 = LDG(g_vy, n1); 
+            vz1 = LDG(g_vz, n1);
+        }
+
+        for (int i1 = 0; i1 < neighbor_number; ++i1)
+        { 
+
+            int index = i1 * number_of_particles + n1;   
+            int n2 = g_neighbor_list[index];
+            int neighbor_number_2 = g_neighbor_number[n2];
+
+            real x12  = LDG(g_x, n2) - x1;
+            real y12  = LDG(g_y, n2) - y1;
+            real z12  = LDG(g_z, n2) - z1;
+            dev_apply_mic
+            (
+                pbc_x, pbc_y, pbc_z, x12, y12, z12, LDG(g_box_length, 0), 
+                LDG(g_box_length, 1), LDG(g_box_length, 2)
+            );
+
+            real f12x = LDG(g_f12x, index); 
+            real f12y = LDG(g_f12y, index);
+            real f12z = LDG(g_f12z, index);
+            int offset = 0;
+            for (int k = 0; k < neighbor_number_2; ++k)
             {
-                int n3 = g_neighbor_list[n2 + number_of_particles * i2];      
-                if (n3 == n1) { continue; } 
-                real x23 = LDG(g_x, n3) - LDG(g_x, n2);
-                real y23 = LDG(g_y, n3) - LDG(g_y, n2);
-                real z23 = LDG(g_z, n3) - LDG(g_z, n2);
-                dev_apply_mic
-                (
-                    pbc_x, pbc_y, pbc_z, x23, y23, z23, LDG(g_box_length, 0), 
-                    LDG(g_box_length, 1), LDG(g_box_length, 2)
-                );
-                real d23 = sqrt(x23 * x23 + y23 * y23 + z23 * z23);     
-                real fc23, fa23;
-                find_fc(ters0, d23, fc23);
-                find_fa(ters0, d23, fa23);
-                real bp23 = LDG(g_bp, i2 * number_of_particles + n2); 
-                real cos213 = - (x12 * x23 + y12 * y23 + z12 * z23)/(d12 * d23);
-                real g213, gp213;
-                find_g_and_gp(ters0, cos213, g213, gp213);
-                real cos_x = x23 / (d12 * d23) + x12 * cos213 / (d12 * d12);
-                real cos_y = y23 / (d12 * d23) + y12 * cos213 / (d12 * d12);
-                real cos_z = z23 / (d12 * d23) + z12 * cos213 / (d12 * d12);
-                real temp213a=(-bp12*fc12*fa12*fc23-bp23*fc23*fa23*fc12)*gp213;
-                real temp213b= - bp23 * fc23 * fa23 * fcp12 * g213 / d12;
-                f21x += (-x12 * temp213b + temp213a * cos_x)*HALF; 
-                f21y += (-y12 * temp213b + temp213a * cos_y)*HALF;
-                f21z += (-z12 * temp213b + temp213a * cos_z)*HALF;
-            }  
-  
-            // per atom force
-            s_fx[threadIdx.x] += f12x - f21x; 
-            s_fy[threadIdx.x] += f12y - f21y; 
-            s_fz[threadIdx.x] += f12z - f21z;  
+                if (n1 == g_neighbor_list[n2 + number_of_particles * k]) 
+                { 
+                    offset = k; break; 
+                }
+            }
+            index = offset * number_of_particles + n2; 
+            real f21x = LDG(g_f12x, index);
+            real f21y = LDG(g_f12y, index);
+            real f21z = LDG(g_f12z, index);
+    
+            // per atom force 
+            s_fx += f12x - f21x; 
+            s_fy += f12y - f21y; 
+            s_fz += f12z - f21z; 
 
             // per-atom stress
             if (cal_p)
@@ -403,20 +432,18 @@ static __global__ void find_force_tersoff_step2
                     g_fv[index_12 + 11] = LDG(g_vz, n2);
                 }  
             }
-            
         }
 
         // save force
-        g_fx[n1] = s_fx[threadIdx.x]; 
-        g_fy[n1] = s_fy[threadIdx.x]; 
-        g_fz[n1] = s_fz[threadIdx.x];
+        g_fx[n1] = s_fx; 
+        g_fy[n1] = s_fy; 
+        g_fz[n1] = s_fz;
 
         if (cal_p) // save stress and potential
         {
             g_sx[n1] = s1[threadIdx.x]; 
             g_sy[n1] = s2[threadIdx.x]; 
             g_sz[n1] = s3[threadIdx.x];
-            g_potential[n1] = s4[threadIdx.x];
         }
 
         if (cal_j) // save heat current
@@ -427,7 +454,6 @@ static __global__ void find_force_tersoff_step2
             g_h[n1 + 3 * number_of_particles] = s4[threadIdx.x];
             g_h[n1 + 4 * number_of_particles] = s5[threadIdx.x];
         }
-
     }
 }   
 
@@ -473,48 +499,63 @@ void gpu_find_force_tersoff1
     int *label = gpu_data->label;
     int *fv_index = gpu_data->fv_index;
     real *fv = gpu_data->fv;
+
+    real *f12x = gpu_data->f12x; 
+    real *f12y = gpu_data->f12y; 
+    real *f12z = gpu_data->f12z; 
     
     find_force_tersoff_step1<<<grid_size, BLOCK_SIZE_FORCE>>>
     (       
         N, pbc_x, pbc_y, pbc_z, force_model->ters0, 
-        NN, NL, 
-        x, y, z, box_length, b, bp
+        NN, NL, x, y, z, box_length, b, bp
     );
 
     if (para->hac.compute)
     {
-        find_force_tersoff_step2<0, 1, 0><<<grid_size, BLOCK_SIZE_FORCE>>>
+        find_force_tersoff_step2<0><<<grid_size, BLOCK_SIZE_FORCE>>>
+        (
+            N, para->pbc_x, para->pbc_y, para->pbc_z, force_model->ters0, 
+            NN, NL, b, bp, x, y, z, box_length, pe, f12x, f12y, f12z
+        );
+        find_force_tersoff_step3<0, 1, 0><<<grid_size, BLOCK_SIZE_FORCE>>>
         (
             N, para->pbc_x, para->pbc_y, para->pbc_z,
-            force_model->ters0, 
-            NN, NL, 
-            b, bp, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
-            sx, sy, sz, pe, h, 
-            label, fv_index, fv
+            force_model->ters0, NN, NL, 
+            f12x, f12y, f12z, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
+            sx, sy, sz, h, label, fv_index, fv
         );
     }
     else if (para->shc.compute)
     {
-        find_force_tersoff_step2<0, 0, 1><<<grid_size, BLOCK_SIZE_FORCE>>>
+        find_force_tersoff_step2<0><<<grid_size, BLOCK_SIZE_FORCE>>>
+        (
+            N, para->pbc_x, para->pbc_y, para->pbc_z, force_model->ters0, 
+            NN, NL, b, bp, x, y, z, box_length, pe, f12x, f12y, f12z
+        );
+        find_force_tersoff_step3<0, 0, 1><<<grid_size, BLOCK_SIZE_FORCE>>>
         (
             N, para->pbc_x, para->pbc_y, para->pbc_z,
-            force_model->ters0, 
-            NN, NL, 
-            b, bp, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
-            sx, sy, sz, pe, h, label, fv_index, fv
+            force_model->ters0, NN, NL, 
+            f12x, f12y, f12z, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
+            sx, sy, sz, h, label, fv_index, fv
         );
     }
     else
     {
-        find_force_tersoff_step2<1, 0, 0><<<grid_size, BLOCK_SIZE_FORCE>>>
+        find_force_tersoff_step2<1><<<grid_size, BLOCK_SIZE_FORCE>>>
+        (
+            N, para->pbc_x, para->pbc_y, para->pbc_z, force_model->ters0, 
+            NN, NL, b, bp, x, y, z, box_length, pe, f12x, f12y, f12z
+        );
+        find_force_tersoff_step3<1, 0, 0><<<grid_size, BLOCK_SIZE_FORCE>>>
         (
             N, para->pbc_x, para->pbc_y, para->pbc_z,
-            force_model->ters0, 
-            NN, NL, 
-            b, bp, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
-            sx, sy, sz, pe, h, label, fv_index, fv
+            force_model->ters0, NN, NL, 
+            f12x, f12y, f12z, x, y, z, vx, vy, vz, box_length, fx, fy, fz, 
+            sx, sy, sz, h, label, fv_index, fv
         );
     }
+
 }
 
 
