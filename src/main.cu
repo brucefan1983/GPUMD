1463636991c332c791f9c84f371084bc27316788
Zheyong Fan <zheyongfan@163.com>
2018-05-18 16:59:16 +0300

    print more compiling information
    print more compiling information

--- src/main.cu
+++ src/main.cu
@@ -30,23 +30,68 @@ int main(int argc, char *argv[])
     printf("***************************************************************\n");
     printf("*                 Welcome to use GPUMD                        *\n");
     printf("*     (Graphics Processing Units Molecular Dynamics)          *\n");
+    printf("*       (Author:  Zheyong Fan <brucenju@gmail.com>)           *\n");
     printf("***************************************************************\n");
     printf("\n");
     
     printf("\n");
     printf("===============================================================\n");
-    printf("INFO:  Compiled with the following options:\n");
-    #ifdef DEBUG
-        printf("       Debug mode is activated.\n");
-    #else
-        srand(time(NULL));
-        printf("       Debug mode is not activated.\n");
-    #endif
-    #ifdef USE_DP
-        printf("       Use double precision.\n");
-    #else
-        printf("       Use single precision.\n");
-    #endif
+    printf("INFO: Compiled with the following options:\n");
+
+#ifdef DEBUG
+    printf("\n");
+    printf("* Debug mode is activated.\n");
+    printf("  -- There is no randomness in the calculations.\n");
+    printf("  -- Always use the O(N^2) algorithm to build neighbor list.\n");
+    printf("\n");
+#else
+    srand(time(NULL));
+    printf("\n");
+    printf("* Debug mode is not activated.\n");
+    printf("  -- There are some randomnesses in the calculations.\n");
+    printf("\n");
+#endif
+
+#ifdef USE_DP
+    printf("\n");
+    printf("* Use double precision. Slower but more accurate.\n");
+    printf("\n");
+#else
+    printf("\n");
+    printf("* Use single precision. Faster but less accurate.\n");
+    printf("\n");
+#endif
+
+#ifdef USE_LDG
+    printf("\n");
+    printf("* Use the __ldg() function in the force evalulation kernels.\n");
+    printf("  -- This is not supported for compute capability < 3.5.\n");
+    printf("\n");
+#else
+    printf("\n");
+    printf("* Not use the __ldg() function.\n");
+    printf("\n");
+#endif
+
+#ifdef FORCE
+    printf("\n");
+    printf("* Will calculate and output the initial forces.\n");
+    printf("  -- This can be used for lattice dynamics calculations.\n");
+    printf("\n");
+#endif
+
+#ifdef TRICLINIC
+    printf("\n");
+    printf("* Use triclinic box.\n");
+    printf("  -- Currently only for the REBO potential of Mo-S systems.\n");
+    printf("  -- Currently only for NVE and NVT ensembles.\n");
+    printf("\n");
+#else
+    printf("\n");
+    printf("* Use rectangular box.\n");
+    printf("\n");
+#endif
+
     printf("===============================================================\n");
     printf("\n");
     
@@ -95,6 +140,7 @@ int main(int argc, char *argv[])
 
     return EXIT_SUCCESS;
 }
+
 
 
 
