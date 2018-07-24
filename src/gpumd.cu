f9882fd01ebf3dd421a4b9f6a85cb63aa36160e0
Zheyong Fan <zheyongfan@163.com>
2018-07-24 22:59:13 +0300

    correct indentation

--- src/gpumd.cu
+++ src/gpumd.cu
@@ -641,40 +641,40 @@ static char *get_file_contents (char *filename)
 ------------------------------------------------------------------------------*/
 static char *row_find_param (char *s, char *param[], int *num_param)
 {
-	*num_param = 0;
-	int start_new_word = 1, comment_found = 0;
-	if (s == NULL) return NULL;
+    *num_param = 0;
+    int start_new_word = 1, comment_found = 0;
+    if (s == NULL) return NULL;
 
-	while(*s)
-	{
-		if(*s == '\n')
-		{
-			*s = '\0';
-			return s + sizeof(char);
-		}
-		else if (comment_found)
-		{
-			// Do nothing
-		}
-		else if (*s == '#')
-		{
-			*s = '\0';
-			comment_found = 1;
-		}
-		else if(isspace(*s))
-		{
-			*s = '\0';
-			start_new_word = 1;
-		}
-		else if (start_new_word)
-		{
-			param[*num_param] = s;
-			++(*num_param);
-			start_new_word = 0;			
-		}
-		++s;
-	}
-	return NULL;
+    while(*s)
+    {
+        if(*s == '\n')
+        {
+            *s = '\0';
+            return s + sizeof(char);
+        }
+        else if (comment_found)
+        {
+            // Do nothing
+        }
+        else if (*s == '#')
+        {
+            *s = '\0';
+            comment_found = 1;
+        }
+        else if(isspace(*s))
+        {
+            *s = '\0';
+            start_new_word = 1;
+        }
+        else if (start_new_word)
+        {
+            param[*num_param] = s;
+            ++(*num_param);
+            start_new_word = 0;			
+        }
+        ++s;
+    }
+    return NULL;
 }
 
 
