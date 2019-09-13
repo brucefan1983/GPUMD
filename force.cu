022179836144277956971a61d8830ec068044f17
Zheyong Fan <brucenju@gmail.com>
2019-09-13 14:24:56 +0300

    start to implement the force constant potential
    up to 4th order now

---
+++ force.cu
@@ -0,0 +1,530 @@
+/*
+    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*----------------------------------------------------------------------------80
+The driver class calculating force and related quantities.
+------------------------------------------------------------------------------*/
+
+
+#include "force.cuh"
+#include "atom.cuh"
+#include "error.cuh"
+#include "mic.cuh"
+#include "potential.cuh"
+#include "tersoff1989.cuh"
+#include "rebo_mos2.cuh"
+#include "vashishta.cuh"
+#include "tersoff1988.cuh"
+#include "tersoff_modc.cuh"
+#include "tersoff_mini.cuh"
+#include "sw.cuh"
+#include "lj.cuh"
+#include "ri.cuh"
+#include "eam.cuh"
+#include "fcp.cuh"
+#include "measure.cuh"
+
+#define BLOCK_SIZE 128
+
+
+Force::Force(void)
+{
+    for (int m = 0; m < MAX_NUM_OF_POTENTIALS; m++)
+    {
+        potential[m] = NULL;
+    }
+    num_of_potentials = 0;
+    rc_max = ZERO;
+    group_method = -1;
+}
+
+
+Force::~Force(void)
+{
+    for (int m = 0; m < num_of_potentials; m++)
+    {
+        delete potential[m];
+        potential[m] = NULL;
+    }
+    MY_FREE(manybody_participation);
+}
+
+
+int Force::get_number_of_types(FILE *fid_potential)
+{
+    int num_of_types;
+    int count = fscanf(fid_potential, "%d", &num_of_types);
+    if (count != 1)
+    {
+        print_error("Number of types not defined for potential.\n");
+    }
+    return num_of_types;
+}
+
+void Force::valdiate_potential_definitions()
+{
+    for (int i = 0; i < num_kind; i++)
+    {
+        if (potential_participation[i] == 0)
+        {
+            print_error("All atoms must participate in at least "
+                    "one potential.\n");
+        }
+    }
+}
+
+void Force::initialize_potential(char* input_dir, Atom* atom, int m)
+{
+    FILE *fid_potential = my_fopen(file_potential[m], "r");
+    char potential_name[20];
+    int count = fscanf(fid_potential, "%s", potential_name);
+    if (count != 1) 
+    {
+        print_error("reading error for potential file.\n");
+    }
+
+    int num_types = get_number_of_types(fid_potential);
+    int potential_type = 0; // 0 - manybody, 1 - two-body
+    // determine the potential
+    if (strcmp(potential_name, "tersoff_1989") == 0)
+    {
+        potential[m] = new Tersoff1989(fid_potential, atom, num_types);
+    }
+    else if (strcmp(potential_name, "tersoff_1988") == 0)
+    {
+        potential[m] = new Tersoff1988(fid_potential, atom, num_types);
+    }
+    else if (strcmp(potential_name, "tersoff_modc") == 0)
+    {
+        potential[m] = new Tersoff_modc(fid_potential, atom, num_types);
+    }
+    else if (strcmp(potential_name, "tersoff_mini") == 0)
+    {
+        potential[m] = new Tersoff_mini(fid_potential, atom, num_types);
+    }
+    else if (strcmp(potential_name, "sw_1985") == 0)
+    {
+        potential[m] = new SW2(fid_potential, atom, num_types);
+    }
+    else if (strcmp(potential_name, "rebo_mos2") == 0)
+    {
+        potential[m] = new REBO_MOS(atom);
+    }
+    else if (strcmp(potential_name, "eam_zhou_2004") == 0)
+    {
+        potential[m] = new EAM(fid_potential, atom, potential_name);
+    }
+    else if (strcmp(potential_name, "eam_dai_2006") == 0)
+    {
+        potential[m] = new EAM(fid_potential, atom, potential_name);
+    }
+    else if (strcmp(potential_name, "vashishta") == 0)
+    {
+        potential[m] = new Vashishta(fid_potential, atom, 0);
+    }
+    else if (strcmp(potential_name, "vashishta_table") == 0)
+    {
+        potential[m] = new Vashishta(fid_potential, atom, 1);
+    }
+    else if (strcmp(potential_name, "fcp") == 0)
+    {
+        potential[m] = new FCP(fid_potential, input_dir, atom);
+    }
+    else if (strcmp(potential_name, "lj") == 0)
+    {
+        potential[m] = new LJ(fid_potential, num_types,
+                participating_kinds, atom_end[m]-atom_begin[m]+1);
+        potential_type = 1;
+    }
+    else if (strcmp(potential_name, "ri") == 0)
+    {
+        if (!kinds_are_contiguous()) // special case for RI
+        {
+            print_error("Defined types/groups for RI potential must be "
+                    "contiguous and ascending.\n");
+        }
+        potential[m] = new RI(fid_potential);
+        potential_type = 1;
+    }
+    else
+    {
+        print_error("illegal potential model.\n");
+    }
+
+    if (potential_type == 0)
+    {
+        if (atom_end[m] - atom_begin[m] + 1 > num_types)
+        {
+            print_error("Error: types/groups must be listed contiguously.\n");
+        }
+    }
+
+    // check if manybody has sequential types (don't care for two-body)
+    if (potential_type == 0 && !kinds_are_contiguous())
+    {
+        print_error("Defined types/groups for manybody potentials must be "
+                            "contiguous and ascending.\n");
+    }
+
+    potential[m]->N1 = 0;
+    potential[m]->N2 = 0;
+
+    if (group_method > -1)
+    {
+        for (int n = 0; n < atom_begin[m]; ++n)
+        {
+            potential[m]->N1 += atom->group[group_method].cpu_size[n];
+        }
+        for (int n = 0; n <= atom_end[m]; ++n)
+        {
+            potential[m]->N2 += atom->group[group_method].cpu_size[n];
+        }
+    }
+    else
+    {
+        for (int n = 0; n < atom_begin[m]; ++n)
+        {
+            potential[m]->N1 += atom->cpu_type_size[n];
+        }
+        for (int n = 0; n <= atom_end[m]; ++n)
+        {
+            potential[m]->N2 += atom->cpu_type_size[n];
+        }
+    }
+
+    // definition bookkeeping
+    for (int n1 = atom_begin[m]; n1 < atom_end[m]+1; n1++)
+    {
+
+        if (potential_type == 0 && manybody_participation[n1])
+        {
+            print_error("Only a single many-body potential "
+                    "definition is allowed per atom type/group (depending "
+                    "on parse_potential keyword).\n");
+        }
+
+        if (potential_type == 0)
+        {
+            manybody_participation[n1] = 1;
+            potential_participation[n1]++;
+        }
+        else
+        {
+            if (kind_is_participating(n1, m))
+                potential_participation[n1]++;
+        }
+    }
+
+    if (group_method > -1)
+    {
+        printf
+        (
+            "       applies to participating atoms [%d, %d) from group %d to "
+            "group %d.\n", potential[m]->N1, potential[m]->N2, atom_begin[m],
+            atom_end[m]
+        );
+    }
+    else
+    {
+        printf
+        (
+            "       applies to participating atoms [%d, %d) from type %d to "
+            "type %d.\n", potential[m]->N1, potential[m]->N2, atom_begin[m],
+            atom_end[m]
+        );
+    }
+
+
+    fclose(fid_potential);
+}
+
+bool Force::kind_is_participating(int kind, int pot_idx)
+{
+    for (int i = 0; i < (int)participating_kinds.size(); i++)
+    {
+        if(kind == participating_kinds[i]) return true;
+    }
+    return false;
+}
+
+bool Force::kinds_are_contiguous()
+{
+    for (int i = 0; i < (int)participating_kinds.size()-1; i++)
+    {
+        if (participating_kinds[i] + 1 != participating_kinds[i+1])
+            return false;
+    }
+    return true;
+}
+
+void Force::add_potential(char* input_dir, Atom *atom)
+{
+    int m = num_of_potentials-1;
+    initialize_potential(input_dir, atom, m);
+    if (rc_max < potential[m]->rc) rc_max = potential[m]->rc;
+
+    // check the atom types in xyz.in
+    for (int n = potential[m]->N1; n < potential[m]->N2; ++n)
+    {
+        int kind;
+        if (group_method > -1) kind = atom->group[group_method].cpu_label[n];
+        else kind = atom->cpu_type[n];
+
+        if (kind < atom_begin[m] || kind > atom_end[m])
+        {
+            printf("ERROR: type for potential # %d not from %d to %d.",
+                m, atom_begin[m], atom_end[m]);
+            exit(1);
+        }
+    }
+    atom->shift[m] = atom_begin[m];
+    participating_kinds.clear(); // reset after every definition
+}
+
+
+// Construct the local neighbor list from the global one (Kernel)
+static __global__ void gpu_find_neighbor_local
+(
+    int triclinic, int pbc_x, int pbc_y, int pbc_z, 
+    int type_begin, int type_end, int *type,
+    int *group, int group_method,
+    int N, int N1, int N2, real cutoff_square, 
+    const real* __restrict__ box,
+    int *NN, int *NL, int *NN_local, int *NL_local,
+#ifdef USE_LDG
+    const real* __restrict__ x, 
+    const real* __restrict__ y, 
+    const real* __restrict__ z
+#else
+    real *x, real *y, real *z
+#endif
+)
+{
+    //<<<(N - 1) / BLOCK_SIZE + 1, BLOCK_SIZE>>>
+    int n1 = blockIdx.x * blockDim.x + threadIdx.x + N1;
+    int count = 0;
+
+
+    if (n1 >= N1 && n1 < N2)
+    {  
+        int neighbor_number = NN[n1];
+
+        real x1 = LDG(x, n1);   
+        real y1 = LDG(y, n1);
+        real z1 = LDG(z, n1);  
+        for (int i1 = 0; i1 < neighbor_number; ++i1)
+        {   
+            int n2 = NL[n1 + N * i1];
+
+            // only include neighbors with the correct types
+            int type_n2;
+            if (group_method > -1)
+                type_n2 = group[n2];
+            else
+                type_n2 = type[n2];
+            if (type_n2 < type_begin || type_n2 > type_end) continue;
+
+            real x12  = LDG(x, n2) - x1;
+            real y12  = LDG(y, n2) - y1;
+            real z12  = LDG(z, n2) - z1;
+            dev_apply_mic(triclinic, pbc_x, pbc_y, pbc_z, box, x12, y12, z12);
+            real distance_square = x12 * x12 + y12 * y12 + z12 * z12;
+            if (distance_square < cutoff_square)
+            {        
+                NL_local[count * N + n1] = n2;
+                ++count;
+            }
+        }
+        NN_local[n1] = count;
+    }
+}
+
+
+// Construct the local neighbor list from the global one (Wrapper)
+void Force::find_neighbor_local(Atom *atom, int m)
+{
+    int type1 = atom_begin[m];
+    int type2 = atom_end[m];
+    int N = atom->N;
+    int N1 = potential[m]->N1;
+    int N2 = potential[m]->N2;
+    int grid_size = (N2 - N1 - 1) / BLOCK_SIZE + 1; 
+    int triclinic = atom->box.triclinic;
+    int pbc_x = atom->box.pbc_x;
+    int pbc_y = atom->box.pbc_y;
+    int pbc_z = atom->box.pbc_z;
+    int *group = atom->group[group_method].label;
+    int *NN = atom->NN;
+    int *NL = atom->NL;
+    int *NN_local = atom->NN_local;
+    int *NL_local = atom->NL_local;
+    int *type = atom->type; // global type
+    real rc2 = potential[m]->rc * potential[m]->rc;
+    real *x = atom->x;
+    real *y = atom->y;
+    real *z = atom->z;
+    real *box = atom->box.h;
+      
+    gpu_find_neighbor_local<<<grid_size, BLOCK_SIZE>>>
+    (
+        triclinic, pbc_x, pbc_y, pbc_z, type1, type2, type, group,
+        group_method, N, N1, N2, rc2, box, NN, NL, NN_local, NL_local, x, y, z
+    );
+    CUDA_CHECK_KERNEL
+
+}
+
+
+// get the total force
+static __global__ void gpu_sum_force
+(int N, real *g_fx, real *g_fy, real *g_fz, real *g_f)
+{
+    //<<<3, 1024>>>
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+    int number_of_patches = (N - 1) / 1024 + 1; 
+    __shared__ real s_f[1024];
+    s_f[tid] = ZERO;
+
+    switch (bid)
+    {
+        case 0:
+            for (int patch = 0; patch < number_of_patches; ++patch)
+            {
+                int n = tid + patch * 1024;
+                if (n < N) s_f[tid] += g_fx[n];
+            }
+            break;
+        case 1:
+            for (int patch = 0; patch < number_of_patches; ++patch)
+            {
+                int n = tid + patch * 1024;
+                if (n < N) s_f[tid] += g_fy[n];
+            }
+            break;
+        case 2:
+            for (int patch = 0; patch < number_of_patches; ++patch)
+            {
+                int n = tid + patch * 1024;
+                if (n < N) s_f[tid] += g_fz[n];
+            }
+            break;
+    }
+
+    __syncthreads();
+
+    #pragma unroll
+    for (int offset = blockDim.x >> 1; offset > 0; offset >>= 1)
+    {
+        if (tid < offset) { s_f[tid] += s_f[tid + offset]; }
+        __syncthreads();
+    } 
+
+    if (tid ==  0) { g_f[bid] = s_f[0]; }
+}
+
+
+// correct the total force
+static __global__ void gpu_correct_force
+(int N, real *g_fx, real *g_fy, real *g_fz, real *g_f)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N)
+    {
+        g_fx[i] -= g_f[0] / N;
+        g_fy[i] -= g_f[1] / N;
+        g_fz[i] -= g_f[2] / N;
+    }
+}
+
+
+static __global__ void initialize_properties
+(
+    int N, real *g_fx, real *g_fy, real *g_fz, real *g_pe,
+    real *g_sx, real *g_sy, real *g_sz, real *g_h
+)
+{
+    int n1 = blockIdx.x * blockDim.x + threadIdx.x;
+    if (n1 < N)
+    {
+        g_fx[n1] = ZERO;
+        g_fy[n1] = ZERO;
+        g_fz[n1] = ZERO;
+        g_sx[n1] = ZERO;
+        g_sy[n1] = ZERO;
+        g_sz[n1] = ZERO;
+        g_pe[n1] = ZERO;
+        g_h[n1 + 0 * N] = ZERO;
+        g_h[n1 + 1 * N] = ZERO;
+        g_h[n1 + 2 * N] = ZERO;
+        g_h[n1 + 3 * N] = ZERO;
+        g_h[n1 + 4 * N] = ZERO;
+    }
+}
+
+
+static __global__ void initialize_shc_properties(int M, real *g_fv)
+{
+    int n1 = blockIdx.x * blockDim.x + threadIdx.x;
+    if (n1 < M) { g_fv[n1] = ZERO; }
+}
+
+
+void Force::compute(Atom *atom, Measure* measure)
+{
+    initialize_properties<<<(atom->N - 1) / BLOCK_SIZE + 1, BLOCK_SIZE>>>
+    (
+        atom->N, atom->fx, atom->fy, atom->fz, atom->potential_per_atom,
+        atom->virial_per_atom_x, atom->virial_per_atom_y,
+        atom->virial_per_atom_z, atom->heat_per_atom
+    );
+    CUDA_CHECK_KERNEL
+
+    if (measure->shc.compute)
+    {
+        int M = measure->shc.number_of_pairs * 12;
+        initialize_shc_properties<<<(M - 1)/ BLOCK_SIZE + 1, BLOCK_SIZE>>>
+        (M, measure->shc.fv);
+        CUDA_CHECK_KERNEL
+    }
+
+    for (int m = 0; m < num_of_potentials; m++)
+    {
+        // first build a local neighbor list
+        find_neighbor_local(atom, m);
+        // and then calculate the forces and related quantities
+        potential[m]->compute(atom, measure, m);
+    }
+
+    // correct the force when using the HNEMD method
+    if (measure->hnemd.compute)
+    {
+        real *ftot; // total force vector of the system
+        CHECK(cudaMalloc((void**)&ftot, sizeof(real) * 3));
+        gpu_sum_force<<<3, 1024>>>
+        (atom->N, atom->fx, atom->fy, atom->fz, ftot);
+        CUDA_CHECK_KERNEL
+
+        int grid_size = (atom->N - 1) / BLOCK_SIZE + 1;
+        gpu_correct_force<<<grid_size, BLOCK_SIZE>>>
+        (atom->N, atom->fx, atom->fy, atom->fz, ftot);
+        CUDA_CHECK_KERNEL
+
+        CHECK(cudaFree(ftot));
+    }
+}
+
+
---
+++ run.cu
@@ -0,0 +1,408 @@
+/*
+    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*----------------------------------------------------------------------------80
+Run simulation according to the inputs in the run.in file.
+------------------------------------------------------------------------------*/
+
+
+#include "run.cuh"
+#include "force.cuh"
+#include "validate.cuh"
+#include "integrate.cuh"
+#include "ensemble.cuh"
+#include "measure.cuh"
+#include "atom.cuh"
+#include "read_file.cuh"
+#include "parse.cuh"
+#include "error.cuh"
+
+
+Run::Run
+(
+    char* input_dir, Atom* atom, Force* force,
+    Integrate* integrate, Measure* measure
+)
+{
+    run(input_dir, atom, force, integrate, measure, 1);
+
+    if (force->group_method > -1)
+        force->num_kind = atom->group[force->group_method].number;
+    else
+        force->num_kind = atom->number_of_types;
+
+    // initialize bookkeeping data structures
+    ZEROS(force->manybody_participation, int, force->num_kind);
+    ZEROS(force->potential_participation, int, force->num_kind);
+    ZEROS(atom->shift, int, MAX_NUM_OF_POTENTIALS);
+
+    run(input_dir, atom, force, integrate, measure, 0);
+}
+
+
+Run::~Run(void)
+{
+    // nothing
+}
+
+
+// set some default values after each run
+void Run::initialize_run(Atom* atom, Integrate* integrate, Measure* measure)
+{
+    atom->neighbor.update = 0;
+    atom->neighbor.number_of_updates = 0;
+    atom->fixed_group     = -1; // no group has an index of -1
+    integrate->deform_x = 0;
+    integrate->deform_y = 0;
+    integrate->deform_z = 0;
+    measure->compute.compute_temperature  = 0;
+    measure->compute.compute_potential    = 0;
+    measure->compute.compute_force        = 0;
+    measure->compute.compute_virial       = 0;
+    measure->compute.compute_jp           = 0;
+    measure->compute.compute_jk           = 0;
+    measure->shc.compute    = 0;
+    measure->vac.compute_dos= 0;
+    measure->vac.compute_sdc= 0;
+    measure->vac.grouping_method = -1;
+    measure->vac.group		= -1;
+    measure->dos.num_dos_points = -1;
+    measure->hac.compute    = 0;
+    measure->hnemd.compute  = 0;
+    measure->dump_thermo    = 0;
+    measure->dump_restart   = 0;
+    measure->dump_velocity  = 0;
+    measure->dump_force     = 0;
+    measure->dump_potential = 0;
+    measure->dump_virial    = 0;
+    measure->dump_heat      = 0;
+
+    /*
+     * Delete dump_pos if it exists. Ensure that dump_pos is NULL in case
+     * it isn't set in parse. If we don't set to NULL, then we may end up
+     * deleting some random address, corrupting memory.
+     */
+    if (measure->dump_pos)
+    {
+    	delete measure->dump_pos;
+    }
+    measure->dump_pos = NULL;
+
+}
+
+
+void Run::print_velocity_and_potential_error(void)
+{
+    if (0 == number_of_times_potential)
+    { print_error("No 'potential(s)' keyword before run.\n"); }
+    if (0 == number_of_times_velocity)
+    { print_error("No 'velocity' keyword before run.\n"); }
+    else if (1 < number_of_times_velocity)
+    { print_error("Multiple 'velocity' keywords before run.\n"); }
+}
+
+
+void Run::print_velocity_error(void)
+{
+    if (1 < number_of_times_velocity)
+    { print_error("Multiple 'velocity' keywords.\n"); }
+}
+
+
+static void check_run_parameters
+(Atom *atom, Integrate *integrate, Measure *measure)
+{
+    if (atom->box.triclinic == 1 && integrate->type == 11)
+    {
+        print_error("Cannot use triclinic box with NPT ensemble.\n");
+    }
+}
+
+
+static void update_temperature(Atom* atom, Integrate* integrate, int step)
+{
+    if (integrate->ensemble->type >= 1 && integrate->ensemble->type <= 20)
+    {
+        integrate->ensemble->temperature = atom->temperature1 
+            + (atom->temperature2 - atom->temperature1)
+            * real(step) / atom->number_of_steps;
+    }
+}
+
+
+static void print_finished_steps(int step, int number_of_steps)
+{
+    if (number_of_steps < 10) { return; }
+    if ((step + 1) % (number_of_steps / 10) == 0)
+    {
+        printf("    %d steps completed.\n", step + 1);
+    }
+}
+
+
+static void print_time_and_speed(clock_t time_begin, Atom* atom)
+{
+    print_line_1();
+    clock_t time_finish = clock();
+    real time_used = (time_finish - time_begin) / (real) CLOCKS_PER_SEC;
+    printf("Number of neighbor list updates = %d.\n",
+        atom->neighbor.number_of_updates);
+    printf("Time used for this run = %g s.\n", time_used);
+    real run_speed = atom->N * (atom->number_of_steps / time_used);
+    printf("Speed of this run = %g atom*step/second.\n", run_speed);
+    print_line_2();
+}
+
+
+// run a number of steps for a given set of inputs
+static void process_run
+(
+    char *input_dir, Atom *atom, Force *force, Integrate *integrate,
+    Measure *measure
+)
+{
+    integrate->initialize(atom);
+    measure->initialize(input_dir, atom);
+    clock_t time_begin = clock();
+    for (int step = 0; step < atom->number_of_steps; ++step)
+    {
+        atom->step = step;
+        atom->global_time += atom->time_step;
+        if (atom->neighbor.update) { atom->find_neighbor(0); }
+        update_temperature(atom, integrate, step);
+        integrate->compute(atom, force, measure);
+        measure->process(input_dir, atom, integrate, step);
+        print_finished_steps(step, atom->number_of_steps);
+    }
+    print_time_and_speed(time_begin, atom);
+    measure->finalize(input_dir, atom, integrate);
+    integrate->finalize();
+}
+
+
+static void print_start(int check)
+{
+    print_line_1();
+    if (check) { printf("Started checking the inputs in run.in.\n"); }
+    else { printf("Started executing the commands in run.in.\n"); }
+    print_line_2();
+}
+
+
+static void print_finish(int check)
+{
+    print_line_1();
+    if (check) { printf("Finished checking the inputs in run.in.\n"); }
+    else { printf("Finished executing the commands in run.in.\n"); }
+    print_line_2();
+}
+
+
+// do something when the keyword is "potential"
+void Run::add_potential
+(
+    char* input_dir, int is_potential, int check,
+    Atom* atom, Force* force, Measure* measure
+)
+{
+    if (!is_potential) { return; }
+    if (check) { number_of_times_potential++; }
+    else
+    {
+        force->add_potential(input_dir, atom);
+    }
+}
+
+
+// do something when the keyword is "velocity"
+void Run::check_velocity(int is_velocity, int check, Atom* atom)
+{
+    if (!is_velocity) { return; }
+    if (check) { number_of_times_velocity++; }
+    else { atom->initialize_velocity(); }
+}
+
+
+// do something when the keyword is "run"
+void Run::check_run
+(
+    char* input_dir, int is_run, int check, Atom* atom,
+    Force* force, Integrate* integrate, Measure* measure
+)
+{
+    if (!is_run) { return; }
+    if (check)
+    {
+        print_velocity_and_potential_error();
+        check_run_parameters(atom, integrate, measure);
+    }
+    else
+    {
+        force->valdiate_potential_definitions();
+
+        process_run(input_dir, atom, force, integrate, measure);
+    }
+    initialize_run(atom, integrate, measure);
+}
+
+
+// Read and process the inputs from the "run.in" file
+void Run::run
+(
+    char *input_dir, Atom *atom, Force *force, Integrate *integrate,
+    Measure *measure, int check
+)
+{
+    char file_run[200];
+    strcpy(file_run, input_dir);
+    strcat(file_run, "/run.in");
+    char *input = get_file_contents(file_run);
+    char *input_ptr = input; // Keep the pointer in order to free later
+    const int max_num_param = 10; // never use more than 9 parameters
+    force->num_of_potentials = 0;
+    int num_param;
+    char *param[max_num_param];
+    initialize_run(atom, integrate, measure); // set some default values
+    print_start(check);
+    while (input_ptr)
+    {
+        input_ptr = row_find_param(input_ptr, param, &num_param);
+        if (num_param == 0) { continue; } 
+        int is_potential = 0;
+        int is_velocity = 0;
+        int is_run = 0;
+        parse(param, num_param, atom, force, integrate, measure,
+            &is_potential, &is_velocity, &is_run);
+        add_potential(input_dir, is_potential, check, atom, force, measure);
+        check_velocity(is_velocity, check, atom);
+        check_run(input_dir, is_run, check, atom, force, integrate, measure);
+    }
+    print_velocity_error();
+    print_finish(check);
+    MY_FREE(input); // Free the input file contents
+}
+
+
+void Run::parse
+(
+    char **param, int num_param, Atom* atom,
+    Force *force, Integrate *integrate, Measure *measure,
+    int *is_potential,int *is_velocity,int *is_run
+)
+{
+    if (strcmp(param[0], "potential_definition") == 0)
+    {
+        parse_potential_definition(param, num_param, atom, force);
+    }
+    else if (strcmp(param[0], "potential") == 0)
+    {
+        *is_potential = 1;
+        parse_potential(param, num_param, force);
+    }
+    else if (strcmp(param[0], "velocity") == 0)
+    {
+        *is_velocity = 1;
+        parse_velocity(param, num_param, atom);
+    }
+    else if (strcmp(param[0], "ensemble")       == 0)
+    {
+        parse_ensemble(param, num_param, atom, integrate);
+    }
+    else if (strcmp(param[0], "time_step")      == 0)
+    {
+        parse_time_step(param, num_param, atom);
+    }
+    else if (strcmp(param[0], "neighbor")       == 0)
+    {
+        parse_neighbor(param, num_param, atom, force);
+    }
+    else if (strcmp(param[0], "dump_thermo")    == 0)
+    {
+        parse_dump_thermo(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_position")  == 0)
+    {
+        parse_dump_position(param, num_param, measure, atom);
+    }
+    else if (strcmp(param[0], "dump_restart")  == 0)
+    {
+        parse_dump_restart(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_velocity")  == 0)
+    {
+        parse_dump_velocity(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_force")     == 0)
+    {
+        parse_dump_force(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_potential") == 0)
+    {
+        parse_dump_potential(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_virial")    == 0)
+    {
+        parse_dump_virial(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "dump_heat")    == 0)
+    {
+        parse_dump_heat(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "compute_dos")    == 0)
+    {
+        parse_compute_dos(param, num_param, measure, atom->group);
+    }
+    else if (strcmp(param[0], "compute_sdc")    == 0)
+	{
+		parse_compute_sdc(param, num_param, measure, atom->group);
+	}
+    else if (strcmp(param[0], "compute_hac")    == 0)
+    {
+        parse_compute_hac(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "compute_hnemd") == 0)
+    {
+        parse_compute_hnemd(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "compute_shc")    == 0)
+    {
+        parse_compute_shc(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "deform")         == 0)
+    {
+        parse_deform(param, num_param, integrate);
+    }
+    else if (strcmp(param[0], "compute")        == 0)
+    {
+        parse_compute(param, num_param, measure);
+    }
+    else if (strcmp(param[0], "fix")            == 0)
+    {
+        parse_fix(param, num_param, atom);
+    }
+    else if (strcmp(param[0], "run")            == 0)
+    {
+        *is_run = 1;
+        parse_run(param, num_param, atom);
+    }
+    else
+    {
+        printf("Error: '%s' is invalid keyword.\n", param[0]);
+        exit(1);
+    }
+}
+
+
