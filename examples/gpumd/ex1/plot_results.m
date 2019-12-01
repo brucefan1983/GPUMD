5229a3ab089e1ba8a8b9173714000f37f6e847ff
Zheyong Fan <brucenju@gmail.com>
2019-12-01 15:38:56 +0200

    correct time step

--- examples/gpumd/ex1/plot_results.m
+++ examples/gpumd/ex1/plot_results.m
@@ -1,7 +1,7 @@
 clear; close all; font_size = 12;
 load thermo.out;
 
-time = 0.02*(1:length(thermo)); % ps
+time = 0.01*(1:length(thermo)); % ps
 temp=100:100:1000; % temperature points (input)
 NT=length(temp); % number of temperature points
 NC=10; % number of cells in each direction
@@ -14,7 +14,7 @@ subplot(2,2,1);
 plot(time, thermo(:,1), 'linewidth',2);
 xlabel('Time (ps)','fontsize',font_size);
 ylabel('Temperature (K)','fontsize',font_size);
-xlim([0,400]);
+xlim([0,200]);
 ylim([0,1100]);
 set(gca,'fontsize',font_size,'linewidth',1,'ticklength',get(gca,'ticklength')*2);
 title('(a)');
@@ -24,7 +24,7 @@ subplot(2,2,2);
 plot(time, mean(thermo(:,4:6),2), 'linewidth',2);
 xlabel('Time (ps)','fontsize',font_size);
 ylabel('Pressure (GPa)','fontsize',font_size);
-xlim([0,400]);
+xlim([0,200]);
 ylim([-0.1,0.4]);
 set(gca,'fontsize',font_size,'linewidth',1,'ticklength',get(gca,'ticklength')*2);
 title('(b)')
@@ -36,7 +36,7 @@ subplot(2,2,3);
 plot(time, a, 'linewidth',2);
 xlabel('Time (ps)','fontsize',font_size);
 ylabel('a (Angstrom)','fontsize',font_size);
-xlim([0,400]);
+xlim([0,200]);
 ylim([5.43,5.48]);
 set(gca,'fontsize',font_size,'linewidth',1,'ticklength',get(gca,'ticklength')*2);
 title('(c)')
