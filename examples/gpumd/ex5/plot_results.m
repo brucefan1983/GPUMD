6df9a145f274248c1e028690a0c10503f5f60000
Zheyong Fan <brucenju@gmail.com>
2020-01-20 06:08:05 +0800

    add an example using the FCP potential
    just get the initial forces from GPUMD and compare them with the reference values from hiPhive

---
+++ examples/gpumd/ex5/plot_results.m
@@ -0,0 +1,19 @@
+close all; clear;
+load compute.out;
+load forces_hiphive.txt;
+forces_gpumd=compute;
+forces_hiphive=reshape(forces_hiphive,256*3,1).';
+
+figure;
+subplot(1,2,1);
+plot(forces_hiphive,'o');
+hold on;
+plot(forces_gpumd,'x');
+xlabel('force components');
+ylabel('force(eV/A)');
+legend('hiPhive','GPUMD');
+
+subplot(1,2,2);
+plot(forces_hiphive-forces_gpumd,'*');
+xlabel('force components');
+ylabel('force difference (eV/A)');
