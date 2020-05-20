4727b9e648b3f3680803cba9d4e4dad042a5c999
Zheyong Fan <brucenju@gmail.com>
2020-05-21 03:52:05 +0800

    update the FCP example

--- examples/gpumd/ex5/plot_results.m
+++ examples/gpumd/ex5/plot_results.m
@@ -1,8 +1,9 @@
-close all; clear;
+close all; 
+clear;
 load compute.out;
 load forces_hiphive.txt;
 forces_gpumd=compute;
-forces_hiphive=reshape(forces_hiphive,256*3,1).';
+forces_hiphive=reshape(forces_hiphive,108*3,1).';
 
 figure;
 subplot(1,2,1);
@@ -10,7 +11,7 @@ plot(forces_hiphive,'o');
 hold on;
 plot(forces_gpumd,'x');
 xlabel('force components');
-ylabel('force(eV/A)');
+ylabel('force (eV/A)');
 legend('hiPhive','GPUMD');
 
 subplot(1,2,2);
