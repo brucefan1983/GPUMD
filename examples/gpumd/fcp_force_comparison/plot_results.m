54dc26194a1d0f9ef6a807b22161729b9af5ffbe
Zheyong Fan <brucenju@gmail.com>
2020-08-24 02:37:49 +0800

    change example folder names

---
+++ examples/gpumd/fcp_force_comparison/plot_results.m
@@ -0,0 +1,20 @@
+close all; 
+clear;
+load force.out;
+load forces_hiphive.txt;
+forces_gpumd=reshape(force,108*3,1);
+forces_hiphive=reshape(forces_hiphive,108*3,1);
+
+figure;
+subplot(1,2,1);
+plot(forces_hiphive,'o');
+hold on;
+plot(forces_gpumd,'x');
+xlabel('force components');
+ylabel('force (eV/A)');
+legend('hiPhive','GPUMD');
+
+subplot(1,2,2);
+plot(forces_hiphive-forces_gpumd,'*');
+xlabel('force components');
+ylabel('force difference (eV/A)');
