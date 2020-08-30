0a2a2552435c3a889c1ef6287d28bcebce8888fc
Zheyong Fan <brucenju@gmail.com>
2020-08-31 02:56:46 +0800

    improve the MATLAB scripts for plotting shc results

--- examples/gpumd/thermal_transport_nemd_and_hnemd/ballistic/plot_shc.m
+++ examples/gpumd/thermal_transport_nemd_and_hnemd/ballistic/plot_shc.m
@@ -6,18 +6,19 @@ Nw=1000; % fourth parameter
 DT=19; % Temperature difference (K) from checking compute.out
 
 % parameters from model (check your xyz.in file)
-Ly=3*1.42*10; % length in transport direction for the chosen group(A)
+Ly=3*1.42*10; % length in transport direction for the chosen group (A)
 Lx=1.42*sqrt(3)*100; % width (A)
 Lz=3.35; % thickness of graphene (A)
 V=Lx*Ly*Lz; % volume of the chosen group (A^3)
 
-% calculated parameters
+% calculated parameters and results
+% Ref. [1]:  Z. Fan et al., PRB 99, 064308 (2019)
 Nt=Nc*2-1;
 time_in_ps=shc(1:Nt,1); % correlation time t (ps)
-K=sum(shc(1:Nt,2:3),2)/Ly; % K(t) function (eV/ps)
+K=sum(shc(1:Nt,2:3),2)/Ly; % Eq. (18) in Ref. [1] divided by length (eV/ps)
 nu=shc(Nt+1:end,1)/2/pi; % frequency (THz)
-J=sum(shc(Nt+1:end,2:3),2); % spectral heat current (A*eV/ps/THz)
-Gc=1.6e4*J/V/DT; % spectral thermal conductivity (W/m/K/THz)
+J=sum(shc(Nt+1:end,2:3),2); % left part of Eq. (20) in Ref. [1] (A*eV/ps/THz)
+Gc=1.6e4*J/V/DT; % spectral thermal conductance (GW/m^2/K/THz)
 
 % plot results
 figure;
@@ -37,5 +38,5 @@ xlim([0,53]);
 set(gca,'ticklength',get(gca,'ticklength')*3,'xtick',0:10:50);
 title('(b)');
 
-save('Gc','Gc');
-sum(Gc)*(nu(2)-nu(1))
+save('Gc','Gc'); % will be used in the diffusive/plot_shc.m file
+sum(Gc)*(nu(2)-nu(1)) % check consistency
--- examples/gpumd/thermal_transport_nemd_and_hnemd/diffusive/plot_shc.m
+++ examples/gpumd/thermal_transport_nemd_and_hnemd/diffusive/plot_shc.m
@@ -7,23 +7,28 @@ T=300; % Temperature (K)
 Fe=1.0e-5; % driving force parameter (1/A)
 
 % parameters from model (check your xyz.in file)
-Ly=3*1.42*10; % length in transport direction for the chosen group(A)
+Ly=3*1.42*10; % length in transport direction for the chosen group (A)
 Lx=1.42*sqrt(3)*100; % width (A)
 Lz=3.35; % thickness of graphene (A)
 V=Lx*Ly*Lz; % volume of the chosen group (A^3)
 
-% calculated parameters
+% calculated parameters and results
+% Ref. [1]:  Z. Fan et al., PRB 99, 064308 (2019)
 Nt=Nc*2-1;
 time_in_ps=shc(1:Nt,1); % correlation time t (ps)
-K=sum(shc(1:Nt,2:3),2)/Ly; % K(t) function (eV/ps)
+K=sum(shc(1:Nt,2:3),2)/Ly; % Eq. (18) in Ref. [1] divided by length (eV/ps)
 nu=shc(Nt+1:end,1)/2/pi; % frequency (THz)
-J=sum(shc(Nt+1:end,2:3),2); % spectral heat current (A*eV/ps/THz)
-kappa=1.6e3*J/V/T/Fe; % spectral thermal conductivity (W/m/K/THz)
+J=sum(shc(Nt+1:end,2:3),2); % left part of Eq. (20) in Ref. [1] (A*eV/ps/THz)
+kappa=1.6e3*J/V/T/Fe; % left part of Eq. (21) in Ref. [1] (W/m/K/THz)
+load ../ballistic/Gc;
+lambda_i=kappa./Gc; % Eq. (22) in Ref. [1] (nm)
+len=10.^(1:0.1:6); % consider length from 10 nm to 1 mm
+for l=1:length(len)
+    tmp=kappa./(1+lambda_i/len(l)); % Eq. (24) in Ref. [1]
+    k_L(l)=sum(tmp)*(nu(2)-nu(1)); % Eq. (23) in Ref. [1]
+end
 
 % plot results
-load ../ballistic/Gc;
-lambda_i=kappa./Gc;
-
 figure;
 subplot(2,2,1);
 plot(time_in_ps,K,'b-','linewidth',2);
@@ -52,12 +57,6 @@ xlim([0,53]);
 set(gca,'ticklength',get(gca,'ticklength')*3,'xtick',0:10:50);
 title('(c)');
 
-len=10.^(1:0.1:6);
-for l=1:length(len)
-    tmp=kappa./(1+lambda_i/len(l));
-    k_L(l)=sum(tmp)*(nu(2)-nu(1));
-end
-
 subplot(2,2,4);
 semilogx(len/1000,k_L,'b-','linewidth',1.5);
 set(gca,'fontsize',font_size);
@@ -67,4 +66,3 @@ ylim([0,3200]);
 xlim([1.0e-2,10^3]);
 set(gca,'ticklength',get(gca,'ticklength')*3,'xtick',10.^(-2:1:6));
 title('(d)');
-
