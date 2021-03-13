71cdb8c2705348c0c66d3db6dd437156525837d1
Zheyong Fan <brucenju@gmail.com>
2021-03-13 21:38:21 +0800

    change copyright

--- src/nep/nn2b.cuh
+++ src/nep/nn2b.cuh
@@ -1,16 +1,16 @@
 /*
-    Copyright 2019 Zheyong Fan
-    This file is part of GPUGA.
-    GPUGA is free software: you can redistribute it and/or modify
+    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    GPUGA is distributed in the hope that it will be useful,
+    GPUMD is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with GPUGA.  If not, see <http://www.gnu.org/licenses/>.
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
