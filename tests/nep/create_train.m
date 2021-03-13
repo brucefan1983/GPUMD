fee4c375b77f58f09f718ca6bffa61080fab2085
Zheyong Fan <brucenju@gmail.com>
2021-03-14 05:29:50 +0800

    update test results

--- tests/nep/create_train.m
+++ tests/nep/create_train.m
@@ -1,46 +1,22 @@
 clear; close all;
-
 [r0, box0] = find_r0_box0();
-N = size(r0, 1);
-strain = -0.02:0.001:0.02;
-N_strain = length(strain);
-N_force = 10;
-
+Na = size(r0, 1); % number of atoms in one configuration
+Nc = 30; % number of configurations
 fid = fopen('train.in', 'w');
-fprintf(fid,'%d %d\n',N_force + N_strain, N_force);
-for n=1:(N_force + N_strain)
-    fprintf(fid,'%d\n',N);
+fprintf(fid,'%d\n',Nc);
+for nc=1:(Nc)
+    fprintf(fid,'%d\n',Na);
 end
-
-for n = 1 : N_force
-    r = r0 + rand(N, 3) * 0.05 * n;
+energy = zeros(1, Nc);
+virial = zeros(6, Nc);
+for nc = 1 : Nc
+    r = r0 + rand(Na, 3) * 0.01*nc;
     box = box0;
-    [junk_energy, junk_virial, force] = find_E(r, box);
+    [energy(nc), virial(:,nc), force] = find_E(r, box);
+    fprintf(fid,'%f %f %f %f %f %f %f\n',energy(nc), virial(:,nc));
     fprintf(fid,'%f %f %f %f %f %f %f %f %f\n',box);
-    for i = 1 : N
+    for i = 1 : Na
         fprintf(fid,'0 %f %f %f %f %f %f\n',r(i,:), force(i,:));
     end
+
 end
-
-figure;
-plot(force)
-
-energy = zeros(1, N_strain);
-virial = zeros(6, N_strain);
-for n = 1 : N_strain
-    r = r0 * (1 + strain(n));
-    box = box0 * (1 + strain(n));
-    [energy(n), virial(:, n), junk_force] = find_E(r, box);
-    fprintf(fid,'%f %f %f %f %f %f %f\n',energy(n), virial(:,n));
-    fprintf(fid,'%f %f %f %f %f %f %f %f %f\n',box);
-    for i = 1 : N
-        fprintf(fid,'0 %f %f %f\n',r(i,:));
-    end
-end
-fclose(fid);
-
-figure;
-plot(strain, energy/N);
-
-figure;
-plot(strain, virial(1, :)/N);
--- tests/nep/plot_results.m
+++ tests/nep/plot_results.m
@@ -5,28 +5,18 @@ load force.out;
 N=256;
 
 figure;
-subplot(1,3,1);
+subplot(1,2,1);
 plot(force(:,4), force(:,1),'b.');
 xlabel('Training force (eV/$\AA$)','fontsize',12,'interpreter','latex');
 ylabel('NN2B force (eV/$\AA$)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 
-strain = -0.02:0.001:0.02;
-subplot(1,3,2);
-plot(strain, energy(:,2)/N,'ro','linewidth',1);hold on;
-plot(strain, energy(:,1)/N,'bx','linewidth',1);hold on;
+subplot(1,2,2);
+plot(energy(:,2)/N,energy(:,1)/N,'ro','linewidth',1);hold on;
 xlabel('Strain','fontsize',12,'interpreter','latex');
 ylabel('Energy (eV/atom)','fontsize',12,'interpreter','latex');
-legend('Training','NN2B');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 
-subplot(1,3,3);
-plot(strain, virial(1:end/6,2)/N,'ro','linewidth',1);hold on;
-plot(strain, virial(1:end/6,1)/N,'bx','linewidth',1);hold on;
-xlabel('Strain','fontsize',12,'interpreter','latex');
-ylabel('Virial (eV/atom)','fontsize',12,'interpreter','latex');
-legend('Training','NN2B');
-set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 
 
 
