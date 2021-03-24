664b693bd9c8f1ea1305d47fddc8930450b06372
Zheyong Fan <brucenju@gmail.com>
2021-03-24 16:24:13 +0800

    better print message

--- src/nep/fitness.cu
+++ src/nep/fitness.cu
@@ -236,8 +236,7 @@ void Fitness::read_potential(char* input_dir)
     print_error("reading error for potential.in.");
   }
   printf(
-    "many_body: %d neurons, %d radial bases, %d angular bases, %g A to %g A.\n", num_neurons_mb,
-    n_max, L_max, r1_2b, r2_2b);
+    "many_body: %d neurons, n_max = %d, l_max = %d.\n", num_neurons_mb, n_max, L_max, r1_2b, r2_2b);
 
   // old c++11 way:
   potential.reset(new NEP(
