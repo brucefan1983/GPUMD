4d2223b3b7facce2fecfa1f4ac8dc2dc314334fa
Zheyong Fan <brucenju@gmail.com>
2021-03-26 15:07:32 +0800

    less magic numbers

--- src/nep/nep.cuh
+++ src/nep/nep.cuh
@@ -18,6 +18,13 @@
 #include "potential.cuh"
 class Neighbor;
 
+const int MAX_NUM_NEURONS_PER_LAYER = 15;
+const int MAX_NUM_N = 11; // n_max+1 = 10+1
+const int MAX_NUM_L = 3;  // L_max+1 = 2+1
+const int MAX_DIM = MAX_NUM_N * MAX_NUM_L;
+const int MAX_W0_SIZE = MAX_DIM * MAX_NUM_NEURONS_PER_LAYER;
+const int MAX_W1_SIZE = MAX_NUM_NEURONS_PER_LAYER * MAX_NUM_NEURONS_PER_LAYER;
+
 struct NEP_Data {
   GPU_Vector<int> NN3b;       // 3-body neighbor number
   GPU_Vector<int> NL3b;       // 3-body neighbor list
@@ -50,20 +57,20 @@ public:
     float r2 = 0.0f;        // outer cutoff
     float r2inv = 0.0f;     // inverse of the outer cutoff
     float pi_factor = 0.0f; // pi/(r2-r1)
-    int n_max;
-    int L_max;
-    float delta_r = 0.0f;
+    int n_max = 0;          // n = 0, 1, 2, ..., n_max
+    int L_max = 0;          // l = 0, 1, 2, ..., L_max
+    float delta_r = 0.0f;   // r2/n_max
   };
 
   struct ANN {
-    int dim = 3;
-    int num_neurons_per_layer;
-    float w0[270]; // from the input layer to the first hidden layer
-    float b0[10];  // from the input layer to the first hidden layer
-    float w1[100]; // from the first hidden layer to the second hidden layer
-    float b1[10];  // from the first hidden layer to the second hidden layer
-    float w2[10];  // from the second hidden layer to the output layer
-    float b2;      // from the second hidden layer to the output layer
+    int dim = 0;                         // dimension of the descriptor
+    int num_neurons_per_layer = 0;       // number of neurons per hidden layer
+    float w0[MAX_W0_SIZE];               // weight from the input to the first hidden layer
+    float b0[MAX_NUM_NEURONS_PER_LAYER]; // bias for the first hidden layer
+    float w1[MAX_W1_SIZE];               // weight from the first to the second hidden layer
+    float b1[MAX_NUM_NEURONS_PER_LAYER]; // bias for the second hidden layer
+    float w2[MAX_NUM_NEURONS_PER_LAYER]; // weight from the second to the output layer
+    float b2;                            // bias for the output layer
   };
 
   NEP(
@@ -76,8 +83,8 @@ public:
     int num_neurons_mb,
     int n_max,
     int L_max);
-  void initialize(int, int);
-  void update_potential(const float*);
+  void initialize(int N, int MAX_ATOM_NUMBER);
+  void update_potential(const float* parameters);
   void find_force(
     int Nc,
     int N,
@@ -100,5 +107,5 @@ private:
   ANN ann3b;
   ANN annmb;
   NEP_Data nep_data;
-  void update_potential(const float* parameters, const int offset, NEP::ANN& ann);
+  void update_potential(const float* parameters, const int offset, ANN& ann);
 };
