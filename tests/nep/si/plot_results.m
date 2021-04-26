87102e6f9fd4679aa9b2e6fb481b22537139ac68
Zheyong Fan <brucenju@gmail.com>
2021-04-27 00:12:23 +0800

    update nep test

--- tests/nep/si/plot_results.m
+++ tests/nep/si/plot_results.m
@@ -1,24 +1,25 @@
-clear; close all;
+clear; 
 load energy.out;
 load virial.out;
 load force.out; 
-load train.out;
-load potential.out
+load loss.out;
+load ann.out
 
 figure;
-subplot(2,3,1);
-plot(force(:,4:6),force(:,1:3),'.'); hold on;
-plot(-4:0.01:4,-4:0.01:4);
-xlabel('DFT force (eV/$\AA$)','fontsize',12,'interpreter','latex');
-ylabel('NEP force (eV/$\AA$)','fontsize',12,'interpreter','latex');
-set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
-axis tight;
 
-subplot(2,3,2);
+subplot(2,3,1);
 plot(energy(:,2),energy(:,1),'.'); hold on;
 plot(-4.8:0.01:-4.2,-4.8:0.01:-4.2);
 xlabel('DFT energy (eV/atom)','fontsize',12,'interpreter','latex');
 ylabel('NEP energy (eV/atom)','fontsize',12,'interpreter','latex');
+set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
+axis tight;
+
+subplot(2,3,2);
+plot(force(:,4:6),force(:,1:3),'.'); hold on;
+plot(-4:0.01:4,-4:0.01:4);
+xlabel('DFT force (eV/$\AA$)','fontsize',12,'interpreter','latex');
+ylabel('NEP force (eV/$\AA$)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 axis tight;
 
@@ -30,30 +31,35 @@ ylabel('NEP virial (eV/atom)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
 axis tight;
 
-
 subplot(2,3,4);
-loglog(train(:,1),train(:,2),'-'); hold on;
-ylim([0.1,100]);
+loglog(loss(:,1),loss(:,5),'rs-'); hold on;
+loglog(loss(:,1),loss(:,8),'bo-'); hold on;
+axis tight
 xlabel('training step','fontsize',12,'interpreter','latex');
 ylabel('Energy RMSE (meV/atom)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
+legend('Train','Test');
 
 subplot(2,3,5);
-loglog(train(:,1),train(:,3),'-'); hold on;
-ylim([10,1000]);
+loglog(loss(:,1),loss(:,6),'rs-'); hold on;
+loglog(loss(:,1),loss(:,9),'bo-');
+axis tight
 xlabel('training step','fontsize',12,'interpreter','latex');
 ylabel('Force RMSE (meV/A)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
+legend('Train','Test');
 
 subplot(2,3,6);
-loglog(train(:,1),train(:,4),'-'); hold on;
-ylim([1,1000]);
+loglog(loss(:,1),loss(:,7),'rs-'); hold on;
+loglog(loss(:,1),loss(:,10),'bo-');
+axis tight
 xlabel('training step','fontsize',12,'interpreter','latex');
 ylabel('Virial RMSE (meV/atom)','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
+legend('Train','Test');
 
 figure;
-semilogx(potential);
+semilogx(loss(:,1),ann);
 xlabel('training step','fontsize',12,'interpreter','latex');
 ylabel('ANN parameters','fontsize',12,'interpreter','latex');
 set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
