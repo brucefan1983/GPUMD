3f0d1540542b3b3e7d548c5ec2a082125a5fe46a
Zheyong Fan <brucenju@gmail.com>
2021-08-13 04:54:07 +0800

    refactor

--- tools/nep_related/snes_vs_gd/get_training_data.m
+++
@@ -1,3 +0,0 @@
-function [x0,y0]=get_training_data()
-x0=exp(0:0.01:log(3));
-y0=10./x0.^12-10./x0.^6;
--- tools/nep_related/snes_vs_gd/test_snes.m
+++ tools/nep_related/snes_vs_gd/test_snes.m
@@ -1,31 +1,10 @@
 clear; %close all;
-
-[x0,y0]=get_training_data();
-
-% Call my_snes to evolve
-N_neurons = 10;
-dim = N_neurons*(N_neurons+4)+1;
-[best_fitness, elite] = my_snes(x0,y0,dim, 10000);
-num_generations = length(best_fitness);
-
-% Evolution of the best fitness:
+[x0,y0,N_neu,N_par]=get_inputs();
+N_gen=1000;
+[loss] = snes(x0,y0,N_par,N_gen);
 figure
-loglog(1 : num_generations, best_fitness, 'linewidth',2)
+loglog(1:N_gen,loss,'linewidth',2)
 xlabel('Generation','fontsize',12);
 ylabel('Best Fitness','fontsize',12);
-set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
-
-% Evolution of the best solution:
-figure
-semilogx(1 : num_generations, elite)
-xlabel('Generation','fontsize',12);
-ylabel('Best Solution','fontsize',12);
-set(gca,'fontsize',12,'ticklength',get(gca,'ticklength')*2);
-
-% compare with the training set:
-[loss, y] = ann(x0,y0,elite);
-figure;
-plot(x0, y0, 'o'); hold on;
-plot(x0, y(end, :), '-');
-
+set(gca,'fontsize',12);
 
