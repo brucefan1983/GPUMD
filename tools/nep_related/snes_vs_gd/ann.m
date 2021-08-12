413219912195098a821bc2da03e4e91f1c5ab65e
Zheyong Fan <brucenju@gmail.com>
2021-08-13 05:00:47 +0800

    refactor

--- tools/nep_related/snes_vs_gd/ann.m
+++ tools/nep_related/snes_vs_gd/ann.m
@@ -1,21 +1,12 @@
 function [loss, y] = ann(x0,y0,population)
 N_samples = length(x0);
-N_neurons = 10;
+N_neu = 10;
 N_pop = size(population, 1);
 loss = zeros(N_pop, 1);
 y = zeros(N_pop, N_samples);
 for n_pop = 1 : N_pop
     para = population(n_pop, :);
-    u = para(1:N_neurons)';
-    offset = N_neurons;
-    v = reshape(para(offset+1 : offset + N_neurons*N_neurons), N_neurons, N_neurons);
-    offset = offset + N_neurons*N_neurons;
-    w = para(offset+1 : offset + N_neurons);
-    offset = offset + N_neurons;
-    a = para(offset+1 : offset + N_neurons)';
-    offset = offset + N_neurons;
-    b = para(offset+1 : offset + N_neurons)';
-    c = para(end);
+    [u,v,w,a,b,c]=get_nn(N_neu,para);
     for n_sample = 1 : N_samples % loop over the samples
         y(n_pop, n_sample) = w * tanh(v * tanh(u * x0(n_sample) - a) - b) - c;
     end
--- tools/nep_related/snes_vs_gd/sgd.m
+++ tools/nep_related/snes_vs_gd/sgd.m
@@ -1,28 +1,25 @@
 clear; %close all;
-x0 = 1 : 0.01 : 3;
+[x0,y0,N_neu,N_par]=get_inputs();
 N_samples = length(x0);
-U0 = 10./x0.^12 - 10./x0.^6 + rand(1, N_samples) * 0.1;
 U = zeros(1, N_samples);
-N_neurons = 10; % 1-10-10-1 NN
-N_neurons2 = N_neurons * N_neurons;
-N_para = N_neurons * (N_neurons + 4) + 1;
-para = (rand(N_para, 1) - 0.5) * 5;
+N_neurons2 = N_neu * N_neu;
+para = (rand(N_par, 1) - 0.5) * 5;
 para_tmp = para;
-momentum = zeros(N_para, 1);
+momentum = zeros(N_par, 1);
 beta = 0.9;
 learning_rate = 0.01;
 N_steps = 10000;
 rmse_U = zeros(N_steps, 1);
 for step = 1 : N_steps
-    para_d = zeros(N_para, 1);
+    para_d = zeros(N_par, 1);
     para_tmp = para + beta * momentum;
     for n_sample = 1 : N_samples
         % get the parameters:
-        u = para_tmp(1:N_neurons); offset = N_neurons;
-        v = reshape(para_tmp(offset+1 : offset + N_neurons*N_neurons), N_neurons, N_neurons); offset = offset + N_neurons*N_neurons;
-        w = para_tmp(offset+1 : offset + N_neurons).'; offset = offset + N_neurons;
-        a = para_tmp(offset+1 : offset + N_neurons); offset = offset + N_neurons;
-        b = para_tmp(offset+1 : offset + N_neurons);
+        u = para_tmp(1:N_neu); offset = N_neu;
+        v = reshape(para_tmp(offset+1 : offset + N_neu*N_neu), N_neu, N_neu); offset = offset + N_neu*N_neu;
+        w = para_tmp(offset+1 : offset + N_neu).'; offset = offset + N_neu;
+        a = para_tmp(offset+1 : offset + N_neu); offset = offset + N_neu;
+        b = para_tmp(offset+1 : offset + N_neu);
         c = para_tmp(end);
         % propagate the NN:
         y = tanh(u * x0(n_sample) - a);
@@ -32,12 +29,12 @@ for step = 1 : N_steps
         U(n_sample) = w * z - c;
         % calculate the derivatives
         deltaU = U(n_sample) - U0(n_sample);
-        para_d1 = zeros(N_para, 1);
-        para_d1(1:N_neurons) = deltaU * x0(n_sample) * yd .* (v.' * zd .* w.'); offset = N_neurons;
-        para_d1(offset+1 : offset + N_neurons*N_neurons) = reshape(deltaU * (w.' .* zd) * y.', N_neurons*N_neurons, 1); offset = offset + N_neurons*N_neurons;
-        para_d1(offset+1 : offset + N_neurons) = deltaU * z.'; offset = offset + N_neurons;
-        para_d1(offset+1 : offset + N_neurons) = - deltaU * yd .* (v.' * zd .* w.'); offset = offset + N_neurons;
-        para_d1(offset+1 : offset + N_neurons) = - deltaU * w.' .* zd;
+        para_d1 = zeros(N_par, 1);
+        para_d1(1:N_neu) = deltaU * x0(n_sample) * yd .* (v.' * zd .* w.'); offset = N_neu;
+        para_d1(offset+1 : offset + N_neu*N_neu) = reshape(deltaU * (w.' .* zd) * y.', N_neu*N_neu, 1); offset = offset + N_neu*N_neu;
+        para_d1(offset+1 : offset + N_neu) = deltaU * z.'; offset = offset + N_neu;
+        para_d1(offset+1 : offset + N_neu) = - deltaU * yd .* (v.' * zd .* w.'); offset = offset + N_neu;
+        para_d1(offset+1 : offset + N_neu) = - deltaU * w.' .* zd;
         para_d1(end) = - deltaU;
         para_d = para_d + para_d1;
     end
