a00410cdc6313cfd5143d35cd0ea716d28744328
Zheyong Fan <brucenju@gmail.com>
2021-09-21 03:38:22 +0800

    prepare for GPUMD-v2.7

--- src/main_nep/main.cu
+++ src/main_nep/main.cu
@@ -65,7 +65,7 @@ void print_welcome_information(void)
   printf("***************************************************************\n");
   printf("*                 Welcome to use GPUMD                        *\n");
   printf("*    (Graphics Processing Units Molecular Dynamics)           *\n");
-  printf("*                     Version 2.6                             *\n");
+  printf("*                     Version 2.7                             *\n");
   printf("*              This is the nep executable                     *\n");
   printf("* Author:                                                     *\n");
   printf("*     Zheyong Fan <brucenju@gmail.com>                        *\n");
