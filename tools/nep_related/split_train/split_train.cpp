7b9e2c7b9c1c931aaca0b012a9b92e5179194225
Zheyong Fan <brucenju@gmail.com>
2021-10-04 05:31:31 +0800

    update my nep tool

--- tools/nep_related/split_train/split_train.cpp
+++ tools/nep_related/split_train/split_train.cpp
@@ -2,17 +2,20 @@
 compile:
     g++ -O3 split_train.cpp
 run:
-    ./a.out num_batches
+    ./a.out test_size
 */
 
+#include <chrono>
+#include <climits>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <random>
 #include <vector>
 
 // some global variables:
-int num_batches, Nc;
+int Nc, Nc_train, Nc_test;
 struct Structure {
   int num_atom;
   int has_virial;
@@ -71,9 +74,6 @@ void read(FILE* fid)
   // number of configurations
   int count = fscanf(fid, "%d", &Nc);
   PRINT_SCANF_ERROR(count, 1, "reading error for number of configurations in train.in.");
-  if (Nc > 100000) {
-    PRINT_INPUT_ERROR("Number of configurations should <= 100000");
-  }
   printf("Total number of structures = %d.\n", Nc);
   structures.resize(Nc);
 
@@ -83,9 +83,6 @@ void read(FILE* fid)
     PRINT_SCANF_ERROR(count, 2, "reading error for number of atoms and virial flag in train.in.");
     if (structures[nc].num_atom < 1) {
       PRINT_INPUT_ERROR("Number of atoms for one configuration should >= 1.");
-    }
-    if (structures[nc].num_atom > 1024) {
-      PRINT_INPUT_ERROR("Number of atoms for one configuration should <=1024.");
     }
   }
 
@@ -128,62 +125,104 @@ void read(FILE* fid)
       if (structures[nc].atomic_number[na] < 0) {
         PRINT_INPUT_ERROR("Atomic number should >= 0.\n");
       }
-      if (structures[nc].atomic_number[na] == 52) {
-        structures[nc].atomic_number[na] = 0;
-      }
-      if (structures[nc].atomic_number[na] == 82) {
-        structures[nc].atomic_number[na] = 1;
-      }
     }
   }
 }
 
-// a simple sorting function
-static void insertion_sort(float array[], int index[], int n)
+void find_permuted_indices(std::vector<int>& permuted_indices)
 {
-  for (int i = 1; i < n; i++) {
-    float key = array[i];
-    int j = i - 1;
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
-      index[j + 1] = index[j];
-      --j;
+  std::mt19937 rng;
+#ifdef DEBUG
+  rng = std::mt19937(54321);
+#else
+  rng = std::mt19937(std::chrono::system_clock::now().time_since_epoch().count());
+#endif
+  for (int i = 0; i < permuted_indices.size(); ++i) {
+    permuted_indices[i] = i;
+  }
+  std::uniform_int_distribution<int> rand_int(0, INT_MAX);
+  for (int i = 0; i < permuted_indices.size(); ++i) {
+    int j = rand_int(rng) % (permuted_indices.size() - i) + i;
+    int temp = permuted_indices[i];
+    permuted_indices[i] = permuted_indices[j];
+    permuted_indices[j] = temp;
+  }
+}
+
+void shuffle()
+{
+  std::vector<int> configuration_id(structures.size());
+  find_permuted_indices(configuration_id);
+
+  std::vector<Structure> structures_copy(structures.size());
+
+  for (int nc = 0; nc < structures.size(); ++nc) {
+    structures_copy[nc].num_atom = structures[nc].num_atom;
+    structures_copy[nc].has_virial = structures[nc].has_virial;
+    structures_copy[nc].energy = structures[nc].energy;
+    for (int k = 0; k < 6; ++k) {
+      structures_copy[nc].virial[k] = structures[nc].virial[k];
     }
-    array[j + 1] = key;
-    index[j + 1] = i;
+    for (int k = 0; k < 9; ++k) {
+      structures_copy[nc].box[k] = structures[nc].box[k];
+    }
+    structures_copy[nc].atomic_number.resize(structures[nc].num_atom);
+    structures_copy[nc].x.resize(structures[nc].num_atom);
+    structures_copy[nc].y.resize(structures[nc].num_atom);
+    structures_copy[nc].z.resize(structures[nc].num_atom);
+    structures_copy[nc].fx.resize(structures[nc].num_atom);
+    structures_copy[nc].fy.resize(structures[nc].num_atom);
+    structures_copy[nc].fz.resize(structures[nc].num_atom);
+    for (int na = 0; na < structures[nc].num_atom; ++na) {
+      structures_copy[nc].atomic_number[na] = structures[nc].atomic_number[na];
+      structures_copy[nc].x[na] = structures[nc].x[na];
+      structures_copy[nc].y[na] = structures[nc].y[na];
+      structures_copy[nc].z[na] = structures[nc].z[na];
+      structures_copy[nc].fx[na] = structures[nc].fx[na];
+      structures_copy[nc].fy[na] = structures[nc].fy[na];
+      structures_copy[nc].fz[na] = structures[nc].fz[na];
+    }
+  }
+
+  for (int nc = 0; nc < structures.size(); ++nc) {
+    structures[nc].num_atom = structures_copy[configuration_id[nc]].num_atom;
+    structures[nc].has_virial = structures_copy[configuration_id[nc]].has_virial;
+    structures[nc].energy = structures_copy[configuration_id[nc]].energy;
+    for (int k = 0; k < 6; ++k) {
+      structures[nc].virial[k] = structures_copy[configuration_id[nc]].virial[k];
+    }
+    for (int k = 0; k < 9; ++k) {
+      structures[nc].box[k] = structures_copy[configuration_id[nc]].box[k];
+    }
+    structures[nc].atomic_number.resize(structures[nc].num_atom);
+    structures[nc].x.resize(structures[nc].num_atom);
+    structures[nc].y.resize(structures[nc].num_atom);
+    structures[nc].z.resize(structures[nc].num_atom);
+    structures[nc].fx.resize(structures[nc].num_atom);
+    structures[nc].fy.resize(structures[nc].num_atom);
+    structures[nc].fz.resize(structures[nc].num_atom);
+    for (int na = 0; na < structures[nc].num_atom; ++na) {
+      structures[nc].atomic_number[na] = structures_copy[configuration_id[nc]].atomic_number[na];
+      structures[nc].x[na] = structures_copy[configuration_id[nc]].x[na];
+      structures[nc].y[na] = structures_copy[configuration_id[nc]].y[na];
+      structures[nc].z[na] = structures_copy[configuration_id[nc]].z[na];
+      structures[nc].fx[na] = structures_copy[configuration_id[nc]].fx[na];
+      structures[nc].fy[na] = structures_copy[configuration_id[nc]].fy[na];
+      structures[nc].fz[na] = structures_copy[configuration_id[nc]].fz[na];
+    }
   }
 }
 
-// sort the sturctures according to the per-atom energy
-void sort()
+// write sturctures to train.out or test.out
+void write(FILE* fid, int nc1, int nc2)
 {
-  energy_array.resize(Nc);
-  index_array.resize(Nc);
-  for (int nc = 0; nc < Nc; ++nc) {
-    energy_array[nc] = structures[nc].energy / structures[nc].num_atom;
-    index_array[nc] = nc;
-  }
-  insertion_sort(energy_array.data(), index_array.data(), Nc);
-}
+  fprintf(fid, "%d\n", nc2 - nc1);
 
-// write the sturctures for a batch
-void write(FILE* fid, int batch)
-{
-  int Nc_batch = 0;
-  for (int nc_old = batch; nc_old < structures.size(); nc_old += num_batches) {
-    Nc_batch++;
-  }
-  printf("Selected number of structures = %d.\n", Nc_batch);
-
-  fprintf(fid, "%d\n", Nc_batch);
-
-  for (int nc_old = batch; nc_old < structures.size(); nc_old += num_batches) {
-    int nc = index_array[nc_old];
+  for (int nc = nc1; nc < nc2; ++nc) {
     fprintf(fid, "%d %d\n", structures[nc].num_atom, structures[nc].has_virial);
   }
 
-  for (int nc_old = batch; nc_old < structures.size(); nc_old += num_batches) {
-    int nc = index_array[nc_old];
+  for (int nc = nc1; nc < nc2; ++nc) {
     if (structures[nc].has_virial) {
       fprintf(
         fid, "%g %g %g %g %g %g %g\n", structures[nc].energy, structures[nc].virial[0],
@@ -207,25 +246,34 @@ void write(FILE* fid, int batch)
 
 int main(int argc, char* argv[])
 {
-  // read in
+  // read Nc_test from command line
   if (argc != 2) {
-    printf("usage: %s batch_size\n", argv[0]);
+    printf("usage: %s test_size\n", argv[0]);
     exit(1);
   }
-  num_batches = atoi(argv[1]);
+  Nc_test = atoi(argv[1]);
 
+  // read train.in
   FILE* fid_in = my_fopen("train.in", "r");
   read(fid_in);
   fclose(fid_in);
 
-  // sort
-  sort();
+  Nc_train = Nc - Nc_test;
+  printf("Number of structures to be output to train.out = %d.\n", Nc_train);
+  printf("Number of structures to be output to test.out = %d.\n", Nc_test);
 
-  // output
-  char outputfile[100];
+  // shuffle the structures
+  shuffle();
+
+  // output train.out
   FILE* fid_train_out = my_fopen("train.out", "w");
-  write(fid_train_out, 0);
+  write(fid_train_out, 0, Nc_train);
   fclose(fid_train_out);
+
+  // output test.out
+  FILE* fid_test_out = my_fopen("test.out", "w");
+  write(fid_test_out, Nc_train, Nc);
+  fclose(fid_test_out);
 
   // Done
   printf("Done.\n");
