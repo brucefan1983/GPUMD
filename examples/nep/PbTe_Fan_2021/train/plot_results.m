05a929ea3e18c48939bda4784614053ae23ca92d
Zheyong Fan <brucenju@gmail.com>
2021-10-21 21:58:40 +0800

    update nep example

--- examples/nep/PbTe_Fan_2021/train/plot_results.m
+++ examples/nep/PbTe_Fan_2021/train/plot_results.m
@@ -1,8 +1,8 @@
 clear; close all;
-load energy.out;load virial.out;load force.out; load loss.out; 
+load energy_test.out;load virial_test.out;load force_test.out; load loss.out; 
 
 figure;
-plot(energy(:,2),energy(:,1),'.','markersize',20); hold on;
+plot(energy_test(:,2),energy_test(:,1),'.','markersize',20); hold on;
 plot(-3.9:0.01:-3.65,-3.9:0.01:-3.65,'linewidth',2);
 xlabel('DFT energy (eV/atom)','fontsize',15,'interpreter','latex');
 ylabel('NEP energy (eV/atom)','fontsize',15,'interpreter','latex');
@@ -10,7 +10,7 @@ set(gca,'fontsize',15,'ticklength',get(gca,'ticklength')*2);
 axis tight;
 
 figure;
-plot(force(:,4:6),force(:,1:3),'.','markersize',20); hold on;
+plot(force_test(:,4:6),force_test(:,1:3),'.','markersize',20); hold on;
 plot(-4:0.01:4,-4:0.01:4,'linewidth',2);
 xlabel('DFT force (eV/$\AA$)','fontsize',15,'interpreter','latex');
 ylabel('NEP force (eV/$\AA$)','fontsize',15,'interpreter','latex');
