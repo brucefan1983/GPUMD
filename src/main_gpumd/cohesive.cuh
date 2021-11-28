23ac75bea25472616caa0dc32a21459e359a69bf
Zheyong Fan <brucenju@gmail.com>
2021-11-28 23:46:52 +0800

    add compute_cohesive command

---
+++ src/main_gpumd/cohesive.cuh
@@ -0,0 +1,46 @@
+/*
+    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "utilities/gpu_vector.cuh"
+#include <vector>
+
+class Box;
+class Group;
+class Neighbor;
+class Force;
+
+class Cohesive
+{
+public:
+  void parse(char** param, int num_param);
+  void compute(
+    char* iput_dir,
+    const Box& box,
+    GPU_Vector<double>& position_per_atom,
+    GPU_Vector<int>& type,
+    std::vector<Group>& group,
+    Neighbor& neighbor,
+    GPU_Vector<double>& potential_per_atom,
+    GPU_Vector<double>& force_per_atom,
+    GPU_Vector<double>& virial_per_atom,
+    Force& force);
+
+private:
+  double start_factor;
+  double end_factor;
+  double delta_factor;
+  int num_points;
+};
