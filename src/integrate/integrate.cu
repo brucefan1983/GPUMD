06f8ddd8bc8104bc1e6ae540a4a8ba299245aa8c
Zheyong Fan <brucenju@gmail.com>
2022-01-24 04:45:26 +0800

    fix a bug for pressure calculation in NVE

--- src/integrate/integrate.cu
+++ src/integrate/integrate.cu
@@ -99,7 +99,7 @@ void Integrate::compute1(
   Atom& atom,
   GPU_Vector<double>& thermo)
 {
-  if (type >= 1 && type <= 20) {
+  if (type <= 20) {
     ensemble->temperature =
       temperature1 + (temperature2 - temperature1) * step_over_number_of_steps;
   }
@@ -117,7 +117,7 @@ void Integrate::compute2(
   Atom& atom,
   GPU_Vector<double>& thermo)
 {
-  if (type >= 1 && type <= 20) {
+  if (type <= 20) {
     ensemble->temperature =
       temperature1 + (temperature2 - temperature1) * step_over_number_of_steps;
   }
