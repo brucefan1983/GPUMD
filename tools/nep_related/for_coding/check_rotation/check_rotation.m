c0034eb2c5297a837d0a1e556108ee15cf81e354
Zheyong Fan <brucenju@gmail.com>
2022-02-12 02:26:47 +0800

    rotational invarance check passed for 4body descriptors

---
+++ tools/nep_related/for_coding/check_rotation/check_rotation.m
@@ -0,0 +1,26 @@
+clear;close all;
+C=get_C();
+N=10;
+xyz=rand(3,N);
+q_ref=get_q(C,xyz(1,:),xyz(2,:),xyz(3,:));
+xyz_rotated=zeros(3,N);
+q_rotated=zeros(1000,1);
+count=0;
+for alpha=linspace(0,2*pi,10)
+    Rx=[1,0,0;0,cos(alpha),-sin(alpha);0,sin(alpha),cos(alpha)];
+    for beta=linspace(0,2*pi,10)
+        Ry=[cos(beta),0,sin(beta);0,1,0;-sin(beta),0,cos(beta)];
+        for gamma=linspace(0,2*pi,10)
+            Rz=[cos(gamma),-sin(gamma),0;sin(gamma),cos(gamma),0;0,0,1];
+            for n=1:N
+                xyz_rotated(:,n)=Rz*Ry*Rx*xyz(:,n);
+            end
+            count=count+1;
+            q_rotated(count)=get_q(C,xyz_rotated(1,:),xyz_rotated(2,:),xyz_rotated(3,:));
+        end
+    end
+end
+
+% should be a constant 1
+figure;
+plot(q_rotated/q_ref)
---
+++ tools/nep_related/for_coding/check_rotation/get_q.m
@@ -0,0 +1,8 @@
+function q=get_q(C,x,y,z)
+r=sqrt(x.^2+y.^2+z.^2);
+s3=sum(3*z.^2-r.^2);
+s4=sum(x.*z);
+s5=sum(y.*z);
+s6=sum(x.^2-y.^2);
+s7=sum(2*x.*y);
+q=C(1)*s3*s3*s3+C(2)*s3*(s4*s4+s5*s5)+C(3)*s3*(s6*s6+s7*s7)+C(4)*s6*(s5*s5-s4*s4)+C(5)*s4*s5*s7;
