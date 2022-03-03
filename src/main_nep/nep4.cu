703fc5e469e42f8ce95ca683a612e843c3dcd129
Zheyong Fan <brucenju@gmail.com>
2022-03-04 05:03:35 +0800

    speed up the kernel apply_gnn_message_passing by 2.5x

--- src/main_nep/nep4.cu
+++ src/main_nep/nep4.cu
@@ -244,51 +244,37 @@ static __global__ void apply_q_scaler(
   }
 }
 
+// Apply the message passing aggregation between neighbors, A*q_theta
 static __global__ void apply_gnn_message_passing(
   const int N,
-  const NEP4::ParaMB paramb,
-  const NEP4::ANN annmb,
-  const NEP4::GNN gnnmb,
+  const NEP4::ParaMB para,
+  const NEP4::ANN ann,
+  const NEP4::GNN gnn,
   const float* __restrict__ g_x12,
   const float* __restrict__ g_y12,
   const float* __restrict__ g_z12,
   const float* __restrict__ g_messages,
   const int* g_NN,
   const int* g_NL,
-  float* __restrict__ gnn_descriptors)
+  float* gnn_descriptors)
 {
-  /* Apply the message passing aggregation between neighbors, A*q_theta */
   int n1 = threadIdx.x + blockIdx.x * blockDim.x;
   if (n1 < N) {
-    int neighbor_number = g_NN[n1]; // Number of neighbors to atom i
-    // get messages for atom i and neighbors
-    float q_theta_i[MAX_DIM] = {0.0f};
-    float q_theta_j[MAX_NEIGHBORS * MAX_DIM] = {0.0f}; // maximum size when all atoms are neighbors
-    float fc_ij[MAX_NEIGHBORS] = {0.0f};
-    for (int d = 0; d < annmb.dim; ++d) {
-      q_theta_i[d] = g_messages[n1 + d * N];
-    }
-    for (int j = 0; j < neighbor_number; ++j) {
-      int index = n1 + N * j;
-      int n2 = g_NL[index];
-      for (int d = 0; d < annmb.dim; ++d) {
-        q_theta_j[j + d * neighbor_number] = g_messages[n2 + d * N];
+    int num_neighbors_of_n1 = g_NN[n1];
+    const int F = ann.dim; // dimension of q_out, for now dim_out = dim_in.
+    for (int nu = 0; nu < F; nu++) {
+      float q_out_tmp = g_messages[n1 + nu * N]; // fc(r_ii) = 1
+      // TODO perhaps normalize weights? Compare Kipf, Welling et al. (2016)
+      for (int j = 0; j < num_neighbors_of_n1; ++j) {
+        int index = n1 + N * j;
+        int n2 = g_NL[index];
+        float r12[3] = {g_x12[index], g_y12[index], g_z12[index]};
+        float d12 = sqrt(r12[0] * r12[0] + r12[1] * r12[1] + r12[2] * r12[2]);
+        float fc12;
+        find_fc(para.rc_angular, para.rcinv_angular, d12, fc12);
+        q_out_tmp += fc12 * g_messages[n2 + nu * N];
       }
-      // Compute weight fc_ij
-      float r12[3] = {g_x12[index], g_y12[index], g_z12[index]};
-      float d12 = sqrt(r12[0] * r12[0] + r12[1] * r12[1] + r12[2] * r12[2]);
-      float fc12;
-      find_fc(paramb.rc_angular, paramb.rcinv_angular, d12, fc12);
-      fc_ij[j] = fc12;
-    }
-
-    // apply gnn to propagate and update descriptors
-    float q_out[MAX_DIM] = {0.0f};
-    apply_gnn_A_q_theta(annmb.dim, neighbor_number, fc_ij, q_theta_i, q_theta_j, q_out);
-    // write propagated descriptor to gnn_descriptors
-    for (int d = 0; d < annmb.dim; ++d) {
-      // printf("n1=%d, q_out[%d]=%f\n", n1, d, q_out[d]);
-      gnn_descriptors[n1 + d * N] = q_out[d];
+      gnn_descriptors[n1 + nu * N] = tanh(q_out_tmp);
     }
   }
 }
