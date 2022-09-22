0b1929338d320f4becb225d7cef356013a7dcc78
Zheyong Fan <brucenju@gmail.com>
2022-09-23 04:55:06 +0800

    prepare for v3.5

--- src/main_nep/main.cu
+++ src/main_nep/main.cu
@@ -67,7 +67,7 @@ void print_welcome_information(void)
   printf("***************************************************************\n");
   printf("*                 Welcome to use GPUMD                        *\n");
   printf("*    (Graphics Processing Units Molecular Dynamics)           *\n");
-  printf("*                    Version 3.4.1                            *\n");
+  printf("*                     Version 3.5                             *\n");
   printf("*              This is the nep executable                     *\n");
   printf("***************************************************************\n");
   printf("\n");
