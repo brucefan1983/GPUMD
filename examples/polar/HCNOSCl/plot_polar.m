095d904884dd50683e26fc04be894e62cff12332
Zheyong Fan <brucenju@gmail.com>
2022-12-29 17:50:25 +0800

    clean up output data for pol and dipole

--- examples/polar/HCNOSCl/plot_polar.m
+++ examples/polar/HCNOSCl/plot_polar.m
@@ -1,15 +1,15 @@
 clear;close all;
 % alphaML results:
-load diag.txt; load test_diag.txt; load offdiag.txt; load test_offdiag.txt;
+load alphaML/diag.txt; load alphaML/test_diag.txt; load alphaML/offdiag.txt; load alphaML/test_offdiag.txt;
 % NEP results:
-load loss.out; load virial_train.out; load virial_test.out; 
+load loss.out; load polarizability_train.out; load polarizability_test.out; 
 
-N=size(virial_train,1)/2;
-train_nep=reshape(virial_train(:,1),N,2);
-train_dft=reshape(virial_train(:,2),N,2);
-N=size(virial_test,1)/2;
-test_nep=reshape(virial_test(:,1),N,2);
-test_dft=reshape(virial_test(:,2),N,2);
+N=size(polarizability_train,1)/2;
+train_nep=reshape(polarizability_train(:,1),N,2);
+train_dft=reshape(polarizability_train(:,2),N,2);
+N=size(polarizability_test,1)/2;
+test_nep=reshape(polarizability_test(:,1),N,2);
+test_dft=reshape(polarizability_test(:,2),N,2);
 
 figure;
 subplot(2,2,1);
@@ -24,7 +24,7 @@ axis tight
 
 subplot(2,2,2);
 plot([diag(:,1);offdiag(:,1)],[diag(:,2);offdiag(:,2)],'o','markersize',6);hold on;
-plot(virial_train(:,2),virial_train(:,1),'.','markersize',10); hold on;
+plot(polarizability_train(:,2),polarizability_train(:,1),'.','markersize',10); hold on;
 plot(-10:0.01:30,-10:0.01:30,'linewidth',2);
 xlabel('Target polarizability/atom','fontsize',15,'interpreter','latex');
 ylabel('Predicted polarizability/atom','fontsize',15,'interpreter','latex');
@@ -34,7 +34,7 @@ axis tight;
 
 subplot(2,2,3);
 plot([test_diag(:,1);test_offdiag(:,1)],[test_diag(:,2);test_offdiag(:,2)],'o','markersize',6);hold on;
-plot(virial_test(:,2),virial_test(:,1),'.','markersize',10); hold on;
+plot(polarizability_test(:,2),polarizability_test(:,1),'.','markersize',10); hold on;
 plot(-10:0.01:20,-10:0.01:20,'linewidth',2);
 xlabel('Target polarizability/atom','fontsize',15,'interpreter','latex');
 ylabel('Predicted polarizability/atom','fontsize',15,'interpreter','latex');
