211305ae85866c15fca83c9dcd4a2586010e5e20
Xu Ke <30561696+Kick-H@users.noreply.github.com>
2023-02-27 10:47:10 +0800

    Create run.sh

---
+++ tools/DataProcessing/rdf-adf/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -u
+
+# First you need to download ovitos: https://www.ovito.org/linux-downloads/
+#   and assign the path to its home directory to OVITO-PTAH.
+# ${OVITO-PTAH}/bin/ovitos rdf_adf_ovito.py
+
+# You can then use Python to output the PDF.
+# python plot_rdf_adf.py
