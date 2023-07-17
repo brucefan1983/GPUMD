dfe9ca57a89e91a56ce22edf6cae4876e1752c1a
bhk <b13548849360@gmail.com>
2023-07-17 17:58:35 +0800

    write the framework of gpu_get_force of ilp

--- src/force/ilp.cu
+++ src/force/ilp.cu
@@ -97,9 +97,84 @@ static __device__ void calc_rep(void)
 
 // force evaluation kernel
 static __global__ void gpu_find_force(
-  ILP_Para ilp)
+  ILP_Para ilp,
+  const int number_of_particles,
+  const int N1,
+  const int N2,
+  const Box box,
+  const int *g_neighbor_number,
+  const int *g_neighbor_list,
+  const int *g_type,
+  const double *__restrict__ g_x,
+  const double *__restrict__ g_y,
+  const double *__restrict__ g_z,
+  double *g_fx,
+  double *g_fy,
+  double *g_fz,
+  double *g_virial,
+  double *g_potential)
 {
-  // TODO
+  int n1 = blockIdx.x * blockDim.x + threadIdx.x + N1; // particle index
+  double s_fx = 0.0;                                   // force_x
+  double s_fy = 0.0;                                   // force_y
+  double s_fz = 0.0;                                   // force_z
+  double s_pe = 0.0;                                   // potential energy
+  double s_sxx = 0.0;                                  // virial_stress_xx
+  double s_sxy = 0.0;                                  // virial_stress_xy
+  double s_sxz = 0.0;                                  // virial_stress_xz
+  double s_syx = 0.0;                                  // virial_stress_yx
+  double s_syy = 0.0;                                  // virial_stress_yy
+  double s_syz = 0.0;                                  // virial_stress_yz
+  double s_szx = 0.0;                                  // virial_stress_zx
+  double s_szy = 0.0;                                  // virial_stress_zy
+  double s_szz = 0.0;                                  // virial_stress_zz
+
+  if (n1 < N2) {
+    int neighor_number = g_neighbor_number[n1];
+    int type1 = g_type[n1];
+    double x1 = g_x[n1];
+    double y1 = g_y[n1];
+    double z1 = g_z[n1];
+
+    for (int i1 = 0; i1 < neighor_number; ++i1) {
+      int n2 = g_neighbor_list[n1 + number_of_particles * i1];
+      int type2 = g_type[n2];
+
+      double x12 = g_x[n2] - x1;
+      double y12 = g_y[n2] - y1;
+      double z12 = g_z[n2] - z1;
+      apply_mic(box, x12, y12, z12);
+
+      calc_normal();
+      calc_vdW();
+      calc_rep();
+    }
+
+    // TODO
+
+    // save force
+    g_fx[n1] += s_fx;
+    g_fy[n1] += s_fy;
+    g_fz[n1] += s_fz;
+
+    // save virial
+    // xx xy xz    0 3 4
+    // yx yy yz    6 1 5
+    // zx zy zz    7 8 2
+    g_virial[n1 + 0 * number_of_particles] += s_sxx;
+    g_virial[n1 + 1 * number_of_particles] += s_syy;
+    g_virial[n1 + 2 * number_of_particles] += s_szz;
+    g_virial[n1 + 3 * number_of_particles] += s_sxy;
+    g_virial[n1 + 4 * number_of_particles] += s_sxz;
+    g_virial[n1 + 5 * number_of_particles] += s_syz;
+    g_virial[n1 + 6 * number_of_particles] += s_syx;
+    g_virial[n1 + 7 * number_of_particles] += s_szx;
+    g_virial[n1 + 8 * number_of_particles] += s_szy;
+
+    // save potential
+    g_potential[n1] += s_pe;
+
+  }
 }
 
 // find force and related quantities
@@ -137,7 +212,23 @@ void ILP::compute(
   }
 #endif
 
-  gpu_find_force<<<grid_size, BLOCK_SIZE_FORCE>>>(ilp_para);
+  gpu_find_force<<<grid_size, BLOCK_SIZE_FORCE>>>(
+    ilp_para,
+    number_of_atoms,
+    N1,
+    N2,
+    box,
+    ilp_data.NN.data(),
+    ilp_data.NL.data(),
+    type.data(),
+    position_per_atom.data(),
+    position_per_atom.data() + number_of_atoms,
+    position_per_atom.data() + number_of_atoms * 2,
+    force_per_atom.data(),
+    force_per_atom.data() + number_of_atoms,
+    force_per_atom.data() + number_of_atoms * 2,
+    virial_per_atom.data(),
+    potential_per_atom.data());
   // TODO
   CUDA_CHECK_KERNEL
 }
