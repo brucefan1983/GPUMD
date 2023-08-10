afabb029ce3dbbc28af6d96ea89c9852074f4f11
Zheyong Fan <brucenju@gmail.com>
2023-08-10 18:51:59 +0800

    prepare data for energy calculator

--- src/mc/mc_ensemble_canonical.cu
+++ src/mc/mc_ensemble_canonical.cu
@@ -53,6 +53,61 @@ static __global__ void get_types(
   }
 }
 
+static __global__ void create_inputs_for_energy_calculator(
+  const int N,
+  const Box box,
+  const float rc_radial_square,
+  const float rc_angular_square,
+  const double* __restrict__ g_x,
+  const double* __restrict__ g_y,
+  const double* __restrict__ g_z,
+  int* g_NN_radial,
+  int* g_NL_radial,
+  int* g_NN_angular,
+  int* g_NL_angular,
+  float* g_x12_radial,
+  float* g_y12_radial,
+  float* g_z12_radial,
+  float* g_x12_angular,
+  float* g_y12_angular,
+  float* g_z12_angular)
+{
+  int n1 = blockIdx.x * blockDim.x + threadIdx.x;
+  if (n1 < N) {
+    double x1 = g_x[n1];
+    double y1 = g_y[n1];
+    double z1 = g_z[n1];
+    int count_radial = 0;
+    int count_angular = 0;
+    for (int n2 = 0; n2 < N; ++n2) {
+      if (n2 == n1) {
+        continue;
+      }
+      double x12 = g_x[n2] - x1;
+      double y12 = g_y[n2] - y1;
+      double z12 = g_z[n2] - z1;
+      apply_mic(box, x12, y12, z12);
+      float distance_square = float(x12 * x12 + y12 * y12 + z12 * z12);
+      if (distance_square < rc_radial_square) {
+        g_NL_radial[count_radial * N + n1] = n2;
+        g_x12_radial[count_radial * N + n1] = float(x12);
+        g_y12_radial[count_radial * N + n1] = float(y12);
+        g_z12_radial[count_radial * N + n1] = float(z12);
+        count_radial++;
+      }
+      if (distance_square < rc_angular_square) {
+        g_NL_angular[count_angular * N + n1] = n2;
+        g_x12_angular[count_angular * N + n1] = float(x12);
+        g_y12_angular[count_angular * N + n1] = float(y12);
+        g_z12_angular[count_angular * N + n1] = float(z12);
+        count_angular++;
+      }
+    }
+    g_NN_radial[n1] = count_radial;
+    g_NN_angular[n1] = count_angular;
+  }
+}
+
 static int get_type(const int i, const int* g_type)
 {
   int type_i = 0;
@@ -92,16 +147,37 @@ void MC_Ensemble_Canonical::compute(Atom& atom, Box& box)
       atom.type.data(),
       type_before.data(),
       type_after.data());
+    CUDA_CHECK_KERNEL
 
-    // std::vector<int> type_before_cpu(atom.number_of_atoms);
-    // std::vector<int> type_after_cpu(atom.number_of_atoms);
-    // type_before.copy_to_host(type_before_cpu.data(), atom.number_of_atoms);
-    // type_after.copy_to_host(type_after_cpu.data(), atom.number_of_atoms);
-    // for (int n = 0; n < atom.number_of_atoms; ++n) {
-    // if (type_before_cpu[n] != type_after_cpu[n])
-    // printf("%d\n", n);
-    // }
+    create_inputs_for_energy_calculator<<<(atom.number_of_atoms - 1) / 64 + 1, 64>>>(
+      atom.number_of_atoms,
+      box,
+      64.0f,
+      16.0f,
+      atom.position_per_atom.data(),
+      atom.position_per_atom.data() + atom.number_of_atoms,
+      atom.position_per_atom.data() + atom.number_of_atoms * 2,
+      NN_radial.data(),
+      NL_radial.data(),
+      NN_angular.data(),
+      NL_angular.data(),
+      x12_radial.data(),
+      y12_radial.data(),
+      z12_radial.data(),
+      x12_angular.data(),
+      y12_angular.data(),
+      z12_angular.data());
+    CUDA_CHECK_KERNEL
 
+    /*
+        std::vector<int> NN_radial_cpu(atom.number_of_atoms);
+        std::vector<int> NN_angular_cpu(atom.number_of_atoms);
+        NN_radial.copy_to_host(NN_radial_cpu.data(), atom.number_of_atoms);
+        NN_angular.copy_to_host(NN_angular_cpu.data(), atom.number_of_atoms);
+        for (int n = 0; n < atom.number_of_atoms; ++n) {
+          printf("%d %d\n", NN_radial_cpu[n], NN_angular_cpu[n]);
+        }
+    */
     exit(1);
   }
 }
