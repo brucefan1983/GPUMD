4ab906a790bf08699e615d1c45d14d5682417ffd
brucefan1983 <brucenju@gmail.com>
2024-01-04 01:59:29 +0800

    no restriction on sigma

--- src/main_nep/snes.cu
+++ src/main_nep/snes.cu
@@ -351,7 +351,6 @@ void SNES::update_mu_and_sigma()
     }
     mu[v] += sigma[v] * gradient_mu;
     sigma[v] *= std::exp(eta_sigma * gradient_sigma);
-    if (sigma[v]>0.1f)sigma[v] = 0.1f;
   }
 }
 
