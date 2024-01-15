c572e14a380cf8f7b9dae4801f194eab2ab8fa3a
psn417 <psn417@icloud.com>
2024-01-15 16:24:44 +0800

    add ensemble nphug

---
+++ src/integrate/ensemble_nphug.cuh
@@ -0,0 +1,40 @@
+/*
+    Copyright 2017 Zheyong Fan, Ville Vierimaa, Mikko Ervasti, and Ari Harju
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "ensemble_mttk.cuh"
+
+class Ensemble_nphug : public Ensemble_MTTK
+{
+  Ensemble_nphug(const char** params, int num_params);
+  Ensemble_nphug(void);
+  virtual ~Ensemble_nphug(void);
+
+  virtual void compute1(
+    const double time_step,
+    const std::vector<Group>& group,
+    Box& box,
+    Atom& atoms,
+    GPU_Vector<double>& thermo);
+
+  virtual void compute2(
+    const double time_step,
+    const std::vector<Group>& group,
+    Box& box,
+    Atom& atoms,
+    GPU_Vector<double>& thermo);
+
+  double t_current = 0, t_start = 0, t_stop = 0, t_target = 0;
+}
