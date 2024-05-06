05fa6b7b0d94ed2dbc90572dccfc3a6114fbcfca
brucefan1983 <brucenju@gmail.com>
2024-05-07 02:36:34 +0800

    better hyperparameters in snes

--- src/main_nep/snes.cu
+++ src/main_nep/snes.cu
@@ -93,8 +93,12 @@ void SNES::initialize_mu_and_sigma(Parameters& para)
   if (fid_restart == NULL) {
     std::uniform_real_distribution<float> r1(0, 1);
     for (int n = 0; n < number_of_variables; ++n) {
-      mu[n] = r1(rng) - 0.5f;
-      sigma[n] = 0.1f;
+      mu[n] = (r1(rng) - 0.5f) * 2.0f;
+      int num = number_of_variables;
+      if (para.version == 4) {
+        num /= para.num_types;
+      }
+      sigma[n] =  (3.0f + std::log(num * 1.0f)) / (5.0f * sqrt(num * 1.0f));
     }
   } else {
     for (int n = 0; n < number_of_variables; ++n) {
