bec67a563c8bf9f47ba609ef3e568afdfe85d46e
bhk <b13548849360@gmail.com>
2024-10-07 17:44:14 +0800

    add ilp_tmd_sw_para in cuh

---
+++ src/force/ilp_tmd_sw.cuh
@@ -0,0 +1,39 @@
+/*
+    Copyright 2017 Zheyong Fan and GPUMD development team
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "potential.cuh"
+#include <stdio.h>
+#include <vector>
+
+// MoS2 MoSe2 WSe2 MoSe2
+#define MAX_TYPE_ILP_TMD_SW 4
+#define CUDA_MAX_NL_TMD 2048
+#define MAX_ILP_NEIGHBOR_TMD 6
+#define MAX_BIG_ILP_NEIGHBOR_TMD 128
+
+struct ILP_TMD_SW_Para {
+  float rcutsq_ilp[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];
+  float d[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];
+  float d_Seff[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];      // d / S_R / r_eff
+  float C_6[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];
+  float z0[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];          // beta
+  float lambda[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];      // alpha / beta
+  float epsilon[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];
+  float C[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];
+  float delta2inv[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];   // 1 / delta ^ 2
+  float S[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];           // scale
+  float rcut_global[MAX_TYPE_ILP_TMD_SW][MAX_TYPE_ILP_TMD_SW];           // scale
+};
