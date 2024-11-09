7a5790e2fabc4528f96b9a4d1ecb379f24734c6f
bhk <b13548849360@gmail.com>
2024-11-09 15:35:12 +0800

    add basic pkg for ilp nep

---
+++ src/force/ilp_nep.cu
@@ -0,0 +1,27 @@
+/*
+    Copyright 2017 Zheyong Fan and GPUMD development team
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*----------------------------------------------------------------------------80
+The class dealing with the interlayer potential(ILP) and neuroevolution 
+potential(NEP).
+TODO:
+------------------------------------------------------------------------------*/
+
+#include "ilp_nep.cuh"
+#include "neighbor.cuh"
+#include "utilities/error.cuh"
+#include "utilities/common.cuh"
+#include "utilities/gpu_macro.cuh"
+
