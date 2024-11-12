a9d5a3f40fb77f50cb70bfc200d0784be3af2b9a
bhk <b13548849360@gmail.com>
2024-11-12 10:39:16 +0800

    add ilp nep construct func but just ilp file reader

--- src/force/ilp_nep.cu
+++ src/force/ilp_nep.cu
@@ -25,3 +25,85 @@ TODO:
 #include "utilities/common.cuh"
 #include "utilities/gpu_macro.cuh"
 
+
+
+ILP_NEP::ILP_NEP(FILE* fid_ilp, FILE* fid_nep_map, int num_types, int num_atoms)
+{
+  // read ILP potential parameter
+  printf("Use %d-element ILP potential with elements:\n", num_types);
+  if (!(num_types >= 1 && num_types <= MAX_TYPE_ILP_NEP)) {
+    PRINT_INPUT_ERROR("Incorrect type number of ILP parameters.\n");
+  }
+  for (int n = 0; n < num_types; ++n) {
+    char atom_symbol[10];
+    int count = fscanf(fid_ilp, "%s", atom_symbol);
+    PRINT_SCANF_ERROR(count, 1, "Reading error for ILP potential.");
+    printf(" %s", atom_symbol);
+  }
+  printf("\n");
+
+  // read parameters
+  float beta, alpha, delta, epsilon, C, d, sR;
+  float reff, C6, S, rcut_ilp, rcut_global;
+  rc = 0.0;
+  for (int n = 0; n < num_types; ++n) {
+    for (int m = 0; m < num_types; ++m) {
+      int count = fscanf(fid_ilp, "%f%f%f%f%f%f%f%f%f%f%f%f", \
+      &beta, &alpha, &delta, &epsilon, &C, &d, &sR, &reff, &C6, &S, \
+      &rcut_ilp, &rcut_global);
+      PRINT_SCANF_ERROR(count, 12, "Reading error for ILP potential.");
+
+      ilp_para.C[n][m] = C;
+      ilp_para.C_6[n][m] = C6;
+      ilp_para.d[n][m] = d;
+      ilp_para.d_Seff[n][m] = d / sR / reff;
+      ilp_para.epsilon[n][m] = epsilon;
+      ilp_para.z0[n][m] = beta;
+      ilp_para.lambda[n][m] = alpha / beta;
+      ilp_para.delta2inv[n][m] = 1.0 / (delta * delta);
+      ilp_para.S[n][m] = S;
+      ilp_para.rcutsq_ilp[n][m] = rcut_ilp * rcut_ilp;
+      ilp_para.rcut_global[n][m] = rcut_global;
+      float meV = 1e-3 * S;
+      ilp_para.C[n][m] *= meV;
+      ilp_para.C_6[n][m] *= meV;
+      ilp_para.epsilon[n][m] *= meV;
+
+      if (rc < rcut_global)
+        rc = rcut_global;
+    }
+  }
+
+  // read NEP parameter
+
+  // initialize neighbor lists and some temp vectors
+  int max_neighbor_number = min(num_atoms, CUDA_MAX_NL_ILP_NEP_CBN);
+  ilp_data.NN.resize(num_atoms);
+  ilp_data.NL.resize(num_atoms * max_neighbor_number);
+  ilp_data.cell_count.resize(num_atoms);
+  ilp_data.cell_count_sum.resize(num_atoms);
+  ilp_data.cell_contents.resize(num_atoms);
+
+  // init ilp neighbor list
+  ilp_data.ilp_NN.resize(num_atoms);
+  ilp_data.ilp_NL.resize(num_atoms * MAX_ILP_NEIGHBOR_CBN);
+  ilp_data.reduce_NL.resize(num_atoms * max_neighbor_number);
+  ilp_data.big_ilp_NN.resize(num_atoms);
+  ilp_data.big_ilp_NL.resize(num_atoms * MAX_BIG_ILP_NEIGHBOR_CBN);
+
+  ilp_data.f12x.resize(num_atoms * max_neighbor_number);
+  ilp_data.f12y.resize(num_atoms * max_neighbor_number);
+  ilp_data.f12z.resize(num_atoms * max_neighbor_number);
+
+  ilp_data.f12x_ilp_neigh.resize(num_atoms * MAX_ILP_NEIGHBOR_CBN);
+  ilp_data.f12y_ilp_neigh.resize(num_atoms * MAX_ILP_NEIGHBOR_CBN);
+  ilp_data.f12z_ilp_neigh.resize(num_atoms * MAX_ILP_NEIGHBOR_CBN);
+
+  // init constant cutoff coeff
+  float h_tap_coeff[8] = \
+    {1.0f, 0.0f, 0.0f, 0.0f, -35.0f, 84.0f, -70.0f, 20.0f};
+  CHECK(gpuMemcpyToSymbol(Tap_coeff_tmd, h_tap_coeff, 8 * sizeof(float)));
+
+  // set ilp_flag to 1
+  ilp_flag = 1;
+}
