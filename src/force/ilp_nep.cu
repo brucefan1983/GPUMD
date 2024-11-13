596aa8d89546dd66a89678439487761ff9677428
bhk <b13548849360@gmail.com>
2024-11-13 22:33:55 +0800

    add modulo func

--- src/force/ilp_nep.cu
+++ src/force/ilp_nep.cu
@@ -610,5 +610,10 @@ static __global__ void ILP_neighbor(
   }
 }
 
+// modulo func to change atom index
+static __device__ __forceinline__ int modulo(int k, int range)
+{
+  return (k + range) % range;
+}
 
 
