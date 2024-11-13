6dadb8f9da33aa8ff6d2082bc348406cef99b477
bhk <b13548849360@gmail.com>
2024-11-13 22:32:54 +0800

    add ilp neigh func

--- src/force/ilp_nep.cu
+++ src/force/ilp_nep.cu
@@ -478,3 +478,137 @@ static __device__ __forceinline__ float calc_dTap(const float r_ij, const float 
 
   return dTap;
 }
+
+// create ILP neighbor list from main neighbor list to calculate normals
+static __global__ void ILP_neighbor(
+  const int number_of_particles,
+  const int N1,
+  const int N2,
+  const Box box,
+  const int *g_neighbor_number,
+  const int *g_neighbor_list,
+  const int *g_type,
+  ILP_Para ilp_para,
+  const double* __restrict__ g_x,
+  const double* __restrict__ g_y,
+  const double* __restrict__ g_z,
+  int *ilp_neighbor_number,
+  int *ilp_neighbor_list,
+  const int *group_label,
+  bool sublayer_flag[MAX_TYPE_ILP_NEP])
+{
+  int n1 = blockIdx.x * blockDim.x + threadIdx.x + N1; // particle index
+
+  if (n1 < N2) {
+    int neighptr[10], check[10], neighsort[10];
+    for (int ll = 0; ll < 10; ++ll) {
+      neighptr[ll] = -1;
+      neighsort[ll] = -1;
+      check[ll] = -1;
+    }
+
+    int count = 0;
+    int neighbor_number = g_neighbor_number[n1];
+    int type1 = g_type[n1];
+    double x1 = g_x[n1];
+    double y1 = g_y[n1];
+    double z1 = g_z[n1];
+
+    for (int i1 = 0; i1 < neighbor_number; ++i1) {
+      int n2 = g_neighbor_list[n1 + number_of_particles * i1];
+      int type2 = g_type[n2];
+
+      double x12 = g_x[n2] - x1;
+      double y12 = g_y[n2] - y1;
+      double z12 = g_z[n2] - z1;
+      apply_mic(box, x12, y12, z12);
+      double d12sq = x12 * x12 + y12 * y12 + z12 * z12;
+      double rcutsq = ilp_para.rcutsq_ilp[type1][type2];
+
+      // if material has sublayer, calc normal in sublayer (same type)
+      if (group_label[n1] == group_label[n2] && d12sq < rcutsq && 
+          (type1 == type2 || !sublayer_flag[type1]) && d12sq != 0) {
+        // ilp_neighbor_list[count++ * number_of_particles + n1] = n2;
+        neighptr[count++] = n2;
+      }
+    }
+
+    // sort orders of neighbors
+    if (sublayer_flag[type1]) {
+      // init neighsort
+      for (int ll = 0; ll < count; ++ll) {
+        neighsort[ll] = neighptr[ll];
+        check[ll] = neighptr[ll];
+      }
+
+      // select the first neighbor of atom n1
+      if (count == MAX_ILP_NEIGHBOR_TMD) {
+        neighsort[0] = neighptr[0];
+        check[0] = -1;
+      } else if (count < MAX_ILP_NEIGHBOR_TMD && count > 0) {
+        for (int jj = 0; jj < count; ++jj) {
+          int j = neighptr[jj];
+          int jtype = g_type[j];
+          int count_temp = 0;
+          for (int ll = 0; ll < count; ++ll) {
+            int l = neighptr[ll];
+            int ltype = g_type[l];
+            if (l == j) continue;
+            double deljx = g_x[l] - g_x[j];
+            double deljy = g_y[l] - g_y[j];
+            double deljz = g_z[l] - g_z[j];
+            apply_mic(box, deljx, deljy, deljz);
+            double rsqlj = deljx * deljx + deljy * deljy + deljz * deljz;
+            if (rsqlj != 0 && rsqlj < ilp_para.rcutsq_ilp[ltype][jtype]) {
+              ++count_temp;
+            }
+          }
+          if (count_temp == 1) {
+            neighsort[0] = neighptr[jj];
+            check[jj] = -1;
+            break;
+          }
+        }
+      } else if (count > MAX_ILP_NEIGHBOR_TMD) {
+        printf("ERROR in ILP NEIGHBOR LIST\n");
+        printf("\n===== ILP neighbor number[%d] is greater than 6 =====\n", count);
+        exit(1);
+      }
+
+      // sort the order of neighbors of atom n1
+      for (int jj = 0; jj < count; ++jj) {
+        int j = neighsort[jj];
+        int jtype = g_type[j];
+        int ll = 0;
+        while (ll < count) {
+          int l = neighptr[ll];
+          if (check[ll] == -1) {
+            ++ll;
+            continue;
+          }
+          int ltype = g_type[l];
+          double deljx = g_x[l] - g_x[j];
+          double deljy = g_y[l] - g_y[j];
+          double deljz = g_z[l] - g_z[j];
+          apply_mic(box, deljx, deljy, deljz);
+          double rsqlj = deljx * deljx + deljy * deljy + deljz * deljz;
+
+          if (abs(rsqlj) >= 1e-6 && rsqlj < ilp_para.rcutsq_ilp[ltype][jtype]) {
+            neighsort[jj + 1] = l;
+            check[ll] = -1;
+            break;
+          }
+          ++ll;
+        }
+      }
+    }
+
+    ilp_neighbor_number[n1] = count;
+    for (int jj = 0; jj < count; ++jj) {
+      ilp_neighbor_list[jj * number_of_particles + n1] = neighsort[jj];
+    }
+  }
+}
+
+
+
