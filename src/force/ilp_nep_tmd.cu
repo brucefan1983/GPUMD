bb3671e6a3b012b4ae72a6cbc7147d34954dfcdc
bhk <b13548849360@gmail.com>
2025-01-08 16:15:57 +0800

    add ilp_nep_tmd construct and destruct funcs

--- src/force/ilp_nep_tmd.cu
+++ src/force/ilp_nep_tmd.cu
@@ -30,3 +30,334 @@ TODO:
 #include <string>
 #include <vector>
 #include <cstring>
+
+#define BLOCK_SIZE_FORCE 128
+
+// there are most 3 intra-layer neighbors for TMDs
+#define NNEI 6
+
+const std::string ELEMENTS[NUM_ELEMENTS] = {
+  "H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na", "Mg", "Al", "Si", "P",  "S",
+  "Cl", "Ar", "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge",
+  "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",  "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+  "In", "Sn", "Sb", "Te", "I",  "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd",
+  "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W",  "Re", "Os", "Ir", "Pt", "Au", "Hg",
+  "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U",  "Np", "Pu"};
+
+ILP_NEP_TMD::ILP_NEP_TMD(FILE* fid_ilp, const char* file_nep, int num_types, int num_atoms)
+{
+  // read ILP TMD potential parameter
+  printf("Use %d-element ILP potential with elements:\n", num_types);
+  if (!(num_types >= 1 && num_types <= IN_MAX_TYPE_ILP_TMD)) {
+    PRINT_INPUT_ERROR("Incorrect type number of ILP_NEP_TMD parameters.\n");
+  }
+  std::vector<std::string> ilp_elements(num_types);
+  for (int n = 0; n < num_types; ++n) {
+    char atom_symbol[10];
+    int count = fscanf(fid_ilp, "%s", atom_symbol);
+    PRINT_SCANF_ERROR(count, 1, "Reading error for ILP_NEP_TMD potential.");
+    printf(" %s", atom_symbol);
+    
+    ilp_elements[n] = atom_symbol;
+  }
+  printf("\n");
+
+  // read ILP group method
+  PRINT_SCANF_ERROR(fscanf(fid_ilp, "%d", &ilp_group_method), 1, 
+  "Reading error for ILP group method.");
+  printf("Use group method %d to identify molecule for ILP.\n", ilp_group_method);
+
+  // read parameters
+  float beta, alpha, delta, epsilon, C, d, sR;
+  float reff, C6, S, rcut_ilp, rcut_global;
+  ilp_rc = 0.0;
+  for (int n = 0; n < num_types; ++n) {
+    for (int m = 0; m < num_types; ++m) {
+      int count = fscanf(fid_ilp, "%f%f%f%f%f%f%f%f%f%f%f%f", \
+      &beta, &alpha, &delta, &epsilon, &C, &d, &sR, &reff, &C6, &S, \
+      &rcut_ilp, &rcut_global);
+      PRINT_SCANF_ERROR(count, 12, "Reading error for ILP_NEP_TMD potential.");
+
+      ilp_para.C[n][m] = C;
+      ilp_para.C_6[n][m] = C6;
+      ilp_para.d[n][m] = d;
+      ilp_para.d_Seff[n][m] = d / sR / reff;
+      ilp_para.epsilon[n][m] = epsilon;
+      ilp_para.z0[n][m] = beta;
+      ilp_para.lambda[n][m] = alpha / beta;
+      ilp_para.delta2inv[n][m] = 1.0 / (delta * delta);
+      ilp_para.S[n][m] = S;
+      ilp_para.rcutsq_ilp[n][m] = rcut_ilp * rcut_ilp;
+      ilp_para.rcut_global[n][m] = rcut_global;
+      float meV = 1e-3 * S;
+      ilp_para.C[n][m] *= meV;
+      ilp_para.C_6[n][m] *= meV;
+      ilp_para.epsilon[n][m] *= meV;
+
+      if (rc_ilp < rcut_global)
+        rc_ilp = rcut_global;
+    }
+  }
+
+
+  // initialize neighbor lists and some temp vectors
+  int max_neighbor_number = min(num_atoms, IN_CUDA_MAX_NL_TMD);
+  ilp_data.NN.resize(num_atoms);
+  ilp_data.NL.resize(num_atoms * max_neighbor_number);
+  ilp_data.cell_count.resize(num_atoms);
+  ilp_data.cell_count_sum.resize(num_atoms);
+  ilp_data.cell_contents.resize(num_atoms);
+
+  // init ilp neighbor list
+  ilp_data.ilp_NN.resize(num_atoms);
+  ilp_data.ilp_NL.resize(num_atoms * IN_MAX_ILP_NEIGHBOR_TMD);
+  ilp_data.reduce_NL.resize(num_atoms * max_neighbor_number);
+  ilp_data.big_ilp_NN.resize(num_atoms);
+  ilp_data.big_ilp_NL.resize(num_atoms * IN_MAX_BIG_ILP_NEIGHBOR_TMD);
+
+  ilp_data.f12x.resize(num_atoms * max_neighbor_number);
+  ilp_data.f12y.resize(num_atoms * max_neighbor_number);
+  ilp_data.f12z.resize(num_atoms * max_neighbor_number);
+
+  ilp_data.f12x_ilp_neigh.resize(num_atoms * IN_MAX_ILP_NEIGHBOR_TMD);
+  ilp_data.f12y_ilp_neigh.resize(num_atoms * IN_MAX_ILP_NEIGHBOR_TMD);
+  ilp_data.f12z_ilp_neigh.resize(num_atoms * IN_MAX_ILP_NEIGHBOR_TMD);
+
+
+  // init constant cutoff coeff
+  float h_tap_coeff[8] = \
+    {1.0f, 0.0f, 0.0f, 0.0f, -35.0f, 84.0f, -70.0f, 20.0f};
+  CHECK(gpuMemcpyToSymbol(IN_Tap_coeff_tmd, h_tap_coeff, 8 * sizeof(float)));
+
+  // set ilp_flag to 1
+  ilp_flag = 1;
+
+
+  std::ifstream input(file_nep);
+  if (!input.is_open()) {
+    std::cout << "Failed to open " << file_nep << std::endl;
+    exit(1);
+  }
+
+  // nep3 1 C
+  std::vector<std::string> tokens = get_tokens(input);
+  if (tokens.size() < 3) {
+    std::cout << "The first line of nep.txt should have at least 3 items." << std::endl;
+    exit(1);
+  }
+  if (tokens[0] == "nep3") {
+    paramb.version = 3;
+  } else if (tokens[0] == "nep4") {
+    paramb.version = 4;
+  } else if (tokens[0] == "nep5") {
+    paramb.version = 5;
+  } else {
+    std::cout << tokens[0]
+              << " is an unsupported NEP model. We only support NEP3 and NEP4 models now."
+              << std::endl;
+    exit(1);
+  }
+  paramb.num_types = get_int_from_token(tokens[1], __FILE__, __LINE__);
+  if (tokens.size() != 2 + paramb.num_types) {
+    std::cout << "The first line of nep.txt should have " << paramb.num_types << " atom symbols."
+              << std::endl;
+    exit(1);
+  }
+
+  if (paramb.num_types == 1) {
+    printf("Use the NEP%d potential with %d atom type.\n", paramb.version, paramb.num_types);
+  } else {
+    printf("Use the NEP%d potential with %d atom types.\n", paramb.version, paramb.num_types);
+  }
+
+  for (int n = 0; n < paramb.num_types; ++n) {
+    if (tokens[2 + n] != ilp_elements[n] || paramb.num_types != num_types) {
+      std::cout << "ILP and NEP potential files must have the same element list\n"
+                << "Element list size of ILP is " << num_types << " NEP is " << paramb.num_types << std::endl
+                << "Element " << n << " in ILP is " << ilp_elements[n] << " in NEP is " << tokens[2 + n] << std::endl;
+      exit(1);
+    }
+    int atomic_number = 0;
+    for (int m = 0; m < NUM_ELEMENTS; ++m) {
+      if (tokens[2 + n] == ELEMENTS[m]) {
+        atomic_number = m + 1;
+        break;
+      }
+    }
+    paramb.atomic_numbers[n] = atomic_number - 1;
+    printf("    type %d (%s with Z = %d).\n", n, tokens[2 + n].c_str(), atomic_number);
+  }
+
+
+  // cutoff 4.2 3.7 80 47 1
+  tokens = get_tokens(input);
+  if (tokens.size() != 5 && tokens.size() != 8) {
+    std::cout << "This line should be cutoff rc_radial rc_angular MN_radial MN_angular "
+                 "[radial_factor] [angular_factor] [zbl_factor].\n";
+    exit(1);
+  }
+  paramb.rc_radial = get_double_from_token(tokens[1], __FILE__, __LINE__);
+  paramb.rc_angular = get_double_from_token(tokens[2], __FILE__, __LINE__);
+  printf("    radial cutoff = %g A.\n", paramb.rc_radial);
+  printf("    angular cutoff = %g A.\n", paramb.rc_angular);
+
+  int MN_radial = get_int_from_token(tokens[3], __FILE__, __LINE__);
+  int MN_angular = get_int_from_token(tokens[4], __FILE__, __LINE__);
+  printf("    MN_radial = %d.\n", MN_radial);
+  if (MN_radial > 819) {
+    std::cout << "The maximum number of neighbors exceeds 819. Please reduce this value."
+              << std::endl;
+    exit(1);
+  }
+  paramb.MN_radial = int(ceil(MN_radial * 1.25));
+  paramb.MN_angular = int(ceil(MN_angular * 1.25));
+  printf("    enlarged MN_radial = %d.\n", paramb.MN_radial);
+  printf("    enlarged MN_angular = %d.\n", paramb.MN_angular);
+
+  if (tokens.size() == 8) {
+    paramb.typewise_cutoff_radial_factor = get_double_from_token(tokens[5], __FILE__, __LINE__);
+    paramb.typewise_cutoff_angular_factor = get_double_from_token(tokens[6], __FILE__, __LINE__);
+    paramb.typewise_cutoff_zbl_factor = get_double_from_token(tokens[7], __FILE__, __LINE__);
+    if (paramb.typewise_cutoff_radial_factor > 0.0f) {
+      paramb.use_typewise_cutoff = true;
+    }
+    if (paramb.typewise_cutoff_zbl_factor > 0.0f) {
+      paramb.use_typewise_cutoff_zbl = true;
+    }
+  }
+#ifdef USE_TABLE
+  if (paramb.use_typewise_cutoff) {
+    PRINT_INPUT_ERROR("Cannot use tabulated radial functions with typewise cutoff.");
+  }
+#endif
+
+  // n_max 10 8
+  tokens = get_tokens(input);
+  if (tokens.size() != 3) {
+    std::cout << "This line should be n_max n_max_radial n_max_angular." << std::endl;
+    exit(1);
+  }
+  paramb.n_max_radial = get_int_from_token(tokens[1], __FILE__, __LINE__);
+  paramb.n_max_angular = get_int_from_token(tokens[2], __FILE__, __LINE__);
+  printf("    n_max_radial = %d.\n", paramb.n_max_radial);
+  printf("    n_max_angular = %d.\n", paramb.n_max_angular);
+
+  // basis_size 10 8
+  tokens = get_tokens(input);
+  if (tokens.size() != 3) {
+    std::cout << "This line should be basis_size basis_size_radial basis_size_angular."
+              << std::endl;
+    exit(1);
+  }
+  paramb.basis_size_radial = get_int_from_token(tokens[1], __FILE__, __LINE__);
+  paramb.basis_size_angular = get_int_from_token(tokens[2], __FILE__, __LINE__);
+  printf("    basis_size_radial = %d.\n", paramb.basis_size_radial);
+  printf("    basis_size_angular = %d.\n", paramb.basis_size_angular);
+
+  // l_max
+  tokens = get_tokens(input);
+  if (tokens.size() != 4) {
+    std::cout << "This line should be l_max l_max_3body l_max_4body l_max_5body." << std::endl;
+    exit(1);
+  }
+
+  paramb.L_max = get_int_from_token(tokens[1], __FILE__, __LINE__);
+  printf("    l_max_3body = %d.\n", paramb.L_max);
+  paramb.num_L = paramb.L_max;
+
+  int L_max_4body = get_int_from_token(tokens[2], __FILE__, __LINE__);
+  int L_max_5body = get_int_from_token(tokens[3], __FILE__, __LINE__);
+  printf("    l_max_4body = %d.\n", L_max_4body);
+  printf("    l_max_5body = %d.\n", L_max_5body);
+  if (L_max_4body == 2) {
+    paramb.num_L += 1;
+  }
+  if (L_max_5body == 1) {
+    paramb.num_L += 1;
+  }
+
+  paramb.dim_angular = (paramb.n_max_angular + 1) * paramb.num_L;
+
+  // ANN
+  tokens = get_tokens(input);
+  if (tokens.size() != 3) {
+    std::cout << "This line should be ANN num_neurons 0." << std::endl;
+    exit(1);
+  }
+  annmb.num_neurons1 = get_int_from_token(tokens[1], __FILE__, __LINE__);
+  annmb.dim = (paramb.n_max_radial + 1) + paramb.dim_angular;
+  nep_model_type = paramb.model_type;
+  if (paramb.model_type == 3) {
+    annmb.dim += 1;
+  }
+  printf("    ANN = %d-%d-1.\n", annmb.dim, annmb.num_neurons1);
+
+  // calculated parameters:
+  rc = paramb.rc_radial; // largest cutoff
+  paramb.rcinv_radial = 1.0f / paramb.rc_radial;
+  paramb.rcinv_angular = 1.0f / paramb.rc_angular;
+  paramb.num_types_sq = paramb.num_types * paramb.num_types;
+
+  if (paramb.version == 3) {
+    annmb.num_para_ann = (annmb.dim + 2) * annmb.num_neurons1 + 1;
+  } else if (paramb.version == 4) {
+    annmb.num_para_ann = (annmb.dim + 2) * annmb.num_neurons1 * paramb.num_types + 1;
+  } else {
+    annmb.num_para_ann = ((annmb.dim + 2) * annmb.num_neurons1 + 1) * paramb.num_types + 1;
+  }
+  if (paramb.model_type == 2) {
+    // Polarizability models have twice as many parameters
+    annmb.num_para_ann *= 2;
+  }
+  printf("    number of neural network parameters = %d.\n", annmb.num_para_ann);
+  int num_para_descriptor =
+    paramb.num_types_sq * ((paramb.n_max_radial + 1) * (paramb.basis_size_radial + 1) +
+                           (paramb.n_max_angular + 1) * (paramb.basis_size_angular + 1));
+  printf("    number of descriptor parameters = %d.\n", num_para_descriptor);
+  annmb.num_para = annmb.num_para_ann + num_para_descriptor;
+  printf("    total number of parameters = %d.\n", annmb.num_para);
+
+  paramb.num_c_radial =
+    paramb.num_types_sq * (paramb.n_max_radial + 1) * (paramb.basis_size_radial + 1);
+
+  // NN and descriptor parameters
+  std::vector<float> parameters(annmb.num_para);
+  for (int n = 0; n < annmb.num_para; ++n) {
+    tokens = get_tokens(input);
+    parameters[n] = get_double_from_token(tokens[0], __FILE__, __LINE__);
+  }
+  nep_data.parameters.resize(annmb.num_para);
+  nep_data.parameters.copy_from_host(parameters.data());
+  update_potential(nep_data.parameters.data(), annmb);
+  for (int d = 0; d < annmb.dim; ++d) {
+    tokens = get_tokens(input);
+    paramb.q_scaler[d] = get_double_from_token(tokens[0], __FILE__, __LINE__);
+  }
+
+
+  nep_data.f12x.resize(num_atoms * paramb.MN_angular);
+  nep_data.f12y.resize(num_atoms * paramb.MN_angular);
+  nep_data.f12z.resize(num_atoms * paramb.MN_angular);
+  nep_data.NN_radial.resize(num_atoms);
+  nep_data.NL_radial.resize(num_atoms * paramb.MN_radial);
+  nep_data.NN_angular.resize(num_atoms);
+  nep_data.NL_angular.resize(num_atoms * paramb.MN_angular);
+  nep_data.Fp.resize(num_atoms * annmb.dim);
+  nep_data.sum_fxyz.resize(num_atoms * (paramb.n_max_angular + 1) * NUM_OF_ABC);
+  nep_data.cell_count.resize(num_atoms);
+  nep_data.cell_count_sum.resize(num_atoms);
+  nep_data.cell_contents.resize(num_atoms);
+  nep_data.cpu_NN_radial.resize(num_atoms);
+  nep_data.cpu_NN_angular.resize(num_atoms);
+
+#ifdef USE_TABLE
+  construct_table(parameters.data());
+  printf("    use tabulated radial functions to speed up.\n");
+#endif
+
+}
+
+ILP_NEP_TMD::~ILP_NEP_TMD(void)
+{
+  // nothing
+}
