cec5beebe7e309b27d538094986c1172114edd23
brucefan1983 <brucenju@gmail.com>
2025-01-20 01:52:48 +0800

    fontsize

--- tools/for_coding/tbmd/test_size_effects.m
+++ tools/for_coding/tbmd/test_size_effects.m
@@ -5,7 +5,7 @@ N_diamond=zeros(Ne,1);
 
 tic;
 for n=1:Ne
-[N_diamond(n),L,r]=find_diamond(n,1.4);
+[N_diamond(n),L,r]=find_diamond(n,1.55);
 [NN,NL]=find_neighbor(N_diamond(n),L,[1 1 1],3,r);
 [energy_diamond(n)]=find_force(N_diamond(n),3,NN,NL,L,[1 1 1],r);
 end
@@ -16,4 +16,4 @@ plot(1:4,energy_diamond./N_diamond,'s-','linewidth',2);hold on;
 xlabel('supercell size');
 ylabel('energy (eV/atom)');
 legend('diamond','graphene');
-set(gca,'fontsize',20);
+set(gca,'fontsize',15);
