2a4251de9257a3a1007e3694c6441695136b38b8
Eric Lindgren <ericlin@chalmers.se>
2025-05-22 16:36:58 +0200

    add atomicAdd for kepler GPUs

--- src/measure/msd.cu
+++ src/measure/msd.cu
@@ -28,6 +28,24 @@ Calculate:
 #include "utilities/read_file.cuh"
 #include <cstring>
 
+
+#ifdef USE_KEPLER
+#if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 600)
+static __device__ __inline__ double atomicAdd(double* address, double val)
+{
+  unsigned long long* address_as_ull = (unsigned long long*)address;
+  unsigned long long old = *address_as_ull, assumed;
+  do {
+    assumed = old;
+    old =
+      atomicCAS(address_as_ull, assumed, __double_as_longlong(val + __longlong_as_double(assumed)));
+
+  } while (assumed != old);
+  return __longlong_as_double(old);
+}
+#endif
+#endif
+
 namespace
 {
 
