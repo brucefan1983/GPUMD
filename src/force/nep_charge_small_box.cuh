ff03702c2500ac66c97e5f7c403395629ba089e4
brucefan1983 <brucenju@gmail.com>
2025-09-27 03:00:05 +0800

    fix small box

--- src/force/nep_charge_small_box.cuh
+++ src/force/nep_charge_small_box.cuh
@@ -203,7 +203,7 @@ static __global__ void find_descriptor_small_box(
         rc = min(
           (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
            COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            ((paramb.charge_mode == 4) ? paramb.typewise_cutoff_angular_factor : paramb.typewise_cutoff_radial_factor),
+            ((paramb.charge_mode >= 4) ? paramb.typewise_cutoff_angular_factor : paramb.typewise_cutoff_radial_factor),
           rc);
       }
       float rcinv = 1.0f / rc;
