11af2b3a048a8dc50a020926067d0a09df3da85a
brucefan1983 <brucenju@gmail.com>
2025-12-11 03:12:56 +0800

    clean

--- src/force/nep_charge.cuh
+++ src/force/nep_charge.cuh
@@ -79,7 +79,6 @@ public:
     int num_types_sq = 0;       // for nep3
     int num_c_radial = 0;       // for nep3
     int num_types = 0;
-    //int atomic_numbers[NUM_ELEMENTS];
   };
 
   struct ANN {
