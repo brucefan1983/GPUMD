97be8618f0627bf91655d5e4f2201c55e67e25cc
brucefan1983 <brucenju@gmail.com>
2025-12-11 01:30:34 +0800

    no typewise cutoff for qnep in nep part

--- src/main_nep/nep_charge.cu
+++ src/main_nep/nep_charge.cu
@@ -36,7 +36,6 @@ static __global__ void gpu_find_neighbor_list(
   const int N,
   const int* Na,
   const int* Na_sum,
-  const bool use_typewise_cutoff,
   const int* g_type,
   const float g_rc_radial,
   const float g_rc_angular,
@@ -66,7 +65,6 @@ static __global__ void gpu_find_neighbor_list(
     float x1 = x[n1];
     float y1 = y[n1];
     float z1 = z[n1];
-    int t1 = g_type[n1];
     int count_radial = 0;
     int count_angular = 0;
     for (int n2 = N1; n2 < N2; ++n2) {
@@ -84,19 +82,8 @@ static __global__ void gpu_find_neighbor_list(
             float z12 = z[n2] + delta_z - z1;
             dev_apply_mic(box, x12, y12, z12);
             float distance_square = x12 * x12 + y12 * y12 + z12 * z12;
-            int t2 = g_type[n2];
             float rc_radial = g_rc_radial;
             float rc_angular = g_rc_angular;
-            if (use_typewise_cutoff) {
-              int z1 = paramb.atomic_numbers[t1];
-              int z2 = paramb.atomic_numbers[t2];
-              rc_radial = min(
-                (COVALENT_RADIUS[z1] + COVALENT_RADIUS[z2]) * paramb.typewise_cutoff_radial_factor,
-                rc_radial);
-              rc_angular = min(
-                (COVALENT_RADIUS[z1] + COVALENT_RADIUS[z2]) * paramb.typewise_cutoff_angular_factor,
-                rc_angular);
-            }
             if (distance_square < rc_radial * rc_radial) {
               NL_radial[count_radial * N + n1] = n2;
               x12_radial[count_radial * N + n1] = x12;
@@ -147,13 +134,6 @@ static __global__ void find_descriptors_radial(
       float fc12;
       int t2 = g_type[n2];
       float rc = (paramb.charge_mode >= 4) ? paramb.rc_angular : paramb.rc_radial;
-      if (paramb.use_typewise_cutoff) {
-        rc = min(
-          (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-           COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            ((paramb.charge_mode >= 4) ? paramb.typewise_cutoff_angular_factor : paramb.typewise_cutoff_radial_factor),
-          rc);
-      }
       float rcinv = 1.0f / rc;
       find_fc(rc, rcinv, d12, fc12);
 
@@ -206,13 +186,6 @@ static __global__ void find_descriptors_angular(
         float fc12;
         int t2 = g_type[n2];
         float rc = paramb.rc_angular;
-        if (paramb.use_typewise_cutoff) {
-          rc = min(
-            (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-             COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-              paramb.typewise_cutoff_angular_factor,
-            rc);
-        }
         float rcinv = 1.0f / rc;
         find_fc(rc, rcinv, d12, fc12);
         float fn12[MAX_NUM_N];
@@ -253,10 +226,7 @@ NEP_Charge::NEP_Charge(
   paramb.version = version;
   paramb.rc_radial = para.rc_radial;
   paramb.rc_angular = para.rc_angular;
-  paramb.use_typewise_cutoff = para.use_typewise_cutoff;
   paramb.use_typewise_cutoff_zbl = para.use_typewise_cutoff_zbl;
-  paramb.typewise_cutoff_radial_factor = para.typewise_cutoff_radial_factor;
-  paramb.typewise_cutoff_angular_factor = para.typewise_cutoff_angular_factor;
   paramb.typewise_cutoff_zbl_factor = para.typewise_cutoff_zbl_factor;
   paramb.num_types = para.num_types;
   paramb.n_max_radial = para.n_max_radial;
@@ -578,13 +548,6 @@ static __global__ void find_force_radial(
       float d12inv = 1.0f / d12;
       float fc12, fcp12;
       float rc = (paramb.charge_mode >= 4) ? paramb.rc_angular : paramb.rc_radial;
-      if (paramb.use_typewise_cutoff) {
-        rc = min(
-          (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-           COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            ((paramb.charge_mode >= 4) ? paramb.typewise_cutoff_angular_factor : paramb.typewise_cutoff_radial_factor),
-          rc);
-      }
       float rcinv = 1.0f / rc;
       find_fc_and_fcp(rc, rcinv, d12, fc12, fcp12);
       float fn12[MAX_NUM_N];
@@ -686,13 +649,6 @@ static __global__ void find_force_angular(
       float fc12, fcp12;
       int t2 = g_type[n2];
       float rc = paramb.rc_angular;
-      if (paramb.use_typewise_cutoff) {
-        rc = min(
-          (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-           COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            paramb.typewise_cutoff_angular_factor,
-          rc);
-      }
       float rcinv = 1.0f / rc;
       find_fc_and_fcp(rc, rcinv, d12, fc12, fcp12);
       float f12[3] = {0.0f};
@@ -761,13 +717,6 @@ static __global__ void find_bec_radial(
       float d12inv = 1.0f / d12;
       float fc12, fcp12;
       float rc = (paramb.charge_mode >= 4) ? paramb.rc_angular : paramb.rc_radial;
-      if (paramb.use_typewise_cutoff) {
-        rc = min(
-          (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-           COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            ((paramb.charge_mode >= 4) ? paramb.typewise_cutoff_angular_factor : paramb.typewise_cutoff_radial_factor),
-          rc);
-      }
       float rcinv = 1.0f / rc;
       find_fc_and_fcp(rc, rcinv, d12, fc12, fcp12);
       float fn12[MAX_NUM_N];
@@ -855,13 +804,6 @@ static __global__ void find_bec_angular(
       float fc12, fcp12;
       int t2 = g_type[n2];
       float rc = paramb.rc_angular;
-      if (paramb.use_typewise_cutoff) {
-        rc = min(
-          (COVALENT_RADIUS[paramb.atomic_numbers[t1]] +
-           COVALENT_RADIUS[paramb.atomic_numbers[t2]]) *
-            paramb.typewise_cutoff_angular_factor,
-          rc);
-      }
       float rcinv = 1.0f / rc;
       find_fc_and_fcp(rc, rcinv, d12, fc12, fcp12);
       float f12[3] = {0.0f};
@@ -1480,7 +1422,6 @@ void NEP_Charge::find_force(
         dataset[device_id].N,
         dataset[device_id].Na.data(),
         dataset[device_id].Na_sum.data(),
-        para.use_typewise_cutoff,
         dataset[device_id].type.data(),
         para.rc_radial,
         para.rc_angular,
