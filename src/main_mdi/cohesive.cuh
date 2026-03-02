c027fb7b8d2b561249954d62d6ca5abfd28c192d
Jaafar Mehrez <32996178+JaafarMehrez@users.noreply.github.com>
2026-03-02 14:15:55 +0800

    GPUMD-MDI capability with gpumd-mdi executable, example of driver/engine paradigm using GPUMD and VASP

---
+++ src/main_mdi/cohesive.cuh
@@ -0,0 +1,61 @@
+/*
+    Copyright 2017 Zheyong Fan and GPUMD development team
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "utilities/gpu_vector.cuh"
+#include <vector>
+
+class Box;
+class Group;
+class Force;
+
+struct D {
+  double data[9];
+};
+
+class Cohesive
+{
+public:
+  void parse(const char** param, int num_param, int type);
+  void compute(
+    Box& box,
+    GPU_Vector<double>& position_per_atom,
+    GPU_Vector<int>& type,
+    std::vector<Group>& group,
+    GPU_Vector<double>& potential_per_atom,
+    GPU_Vector<double>& force_per_atom,
+    GPU_Vector<double>& virial_per_atom,
+    Force& force);
+
+private:
+  void parse_cohesive(const char** param, int num_param);
+  void parse_elastic(const char** param, int num_param);
+  void allocate_memory(const int num_atoms);
+  void compute_D();
+  void output(Box& box);
+  void deform_box(
+    const int N, const D& cpu_d, Box& old_box, Box& new_box, GPU_Vector<double>& position_per_atom);
+  std::vector<double> cpu_potential_total;
+  std::vector<double> cpu_potential_per_atom;
+  std::vector<D> cpu_D;
+  GPU_Vector<double> new_position_per_atom;
+  double strain;
+  double start_factor;
+  double end_factor;
+  double delta_factor;
+  int num_points;
+  int deformation_type; // 0-7 = cohesive, cubic, hexagonal, trigonal, tetragonal, orthorhombic,
+                        // monoclinic, triclinic
+};
---
+++ src/main_mdi/velocity.cuh
@@ -0,0 +1,63 @@
+/*
+    Copyright 2017 Zheyong Fan and GPUMD development team
+    This file is part of GPUMD.
+    GPUMD is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    GPUMD is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "utilities/gpu_vector.cuh"
+#include <vector>
+
+class Group;
+
+class Velocity
+{
+public:
+  bool do_velocity_correction = false;
+  int velocity_correction_interval = 1000;
+  int velocity_correction_group_method = -1;
+
+  void initialize(
+    const bool has_velocity_in_xyz,
+    const double initial_temperature,
+    const std::vector<double>& cpu_mass,
+    const std::vector<double>& cpu_position_per_atom,
+    std::vector<double>& cpu_velocity_per_atom,
+    GPU_Vector<double>& velocity_per_atom,
+    bool use_seed,
+    int seed);
+
+  void correct_velocity(
+    const int step,
+    const std::vector<Group>& group,
+    const std::vector<double>& cpu_mass,
+    GPU_Vector<double>& position_per_atom,
+    std::vector<double>& cpu_position_per_atom,
+    std::vector<double>& cpu_velocity_per_atom,
+    GPU_Vector<double>& velocity_per_atom);
+
+  void finalize();
+
+private:
+  void correct_velocity(
+    const std::vector<double>& cpu_mass,
+    const std::vector<double>& cpu_position_per_atom,
+    std::vector<double>& cpu_velocity_per_atom);
+
+  void scale(
+    const double initial_temperature,
+    const std::vector<double>& cpu_mass,
+    double* cpu_vx,
+    double* cpu_vy,
+    double* cpu_vz);
+};
