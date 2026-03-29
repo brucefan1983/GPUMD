d1e91336626afca5e38f17c7f5bf2f331517aa20
Qing'an Li <chanzylazer@qq.com>
2026-03-29 18:26:40 +0800

    clear llm hallucination

--- src/force/nnap.cu
+++ src/force/nnap.cu
@@ -13,97 +13,25 @@
     along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifdef USE_NNAP
+/**
+ * NNAP interface for GPUMD
+ * @author liqa
+ */
+
 #include "nnap.cuh"
 #include "utilities/error.cuh"
 #include "utilities/gpu_macro.cuh"
-#include <cstring>
-#include <iostream>
-
-#define BLOCK_SIZE_FORCE 128
 
 NNAP::NNAP(const char* filename, int num_atoms)
 {
-  set_nnap_coeff();
-  initialize_nnap(filename);
-
-  nnap_data.NN.resize(num_atoms);
-  nnap_data.NL.resize(num_atoms * MAX_NEIGH_NUM_NNAP); // should define MAX_NEIGH_NUM_NNAP from the NNAP.txt potential file.
-  nnap_data.cell_count.resize(num_atoms);
-  nnap_data.cell_count_sum.resize(num_atoms);
-  nnap_data.cell_contents.resize(num_atoms);
-
-  position_gpu_trans.resize(num_atoms * 3);
-  type_cpu.resize(num_atoms);
-  e_f_v_gpu.resize(num_atoms * (1 + 3 + 9));
-
-  nnap_nl.inum = num_atoms;
-  nnap_nl.ilist.resize(num_atoms);
-  nnap_nl.numneigh.resize(num_atoms);
-  nnap_nl.firstneigh.resize(num_atoms);
-  nnap_nl.neigh_storage.resize(num_atoms * MAX_NEIGH_NUM_NNAP);
+  
+  rc = 6.0; // TODO:
+  neighbor.initialize(rc, num_atoms, 700);
 }
 
 NNAP::~NNAP(void)
 {
   // empty
-}
-
-void NNAP::initialize_nnap(const char* filename)
-{
-  printf("\nInitialize empty NNAP driver by file: %s\n\n", filename);
-  rc = 6.0; // placeholder cutoff, NNAP developers can modify
-}
-
-void NNAP::set_nnap_coeff(void)
-{
-  ener_unit_cvt_factor = 1.0;
-  dist_unit_cvt_factor = 1.0;
-  force_unit_cvt_factor = ener_unit_cvt_factor / dist_unit_cvt_factor;
-  virial_unit_cvt_factor = 1.0;
-}
-
-static __global__ void position_transpose(
-  const double* position,
-  double* position_trans,
-  int N)
-{
-  int n = blockIdx.x * blockDim.x + threadIdx.x;
-  if (n < N) {
-    position_trans[n * 3 + 0] = position[n];
-    position_trans[n * 3 + 1] = position[n + N];
-    position_trans[n * 3 + 2] = position[n + 2 * N];
-  }
-}
-
-void NNAP::nnap_api_compute(
-  int nlocal,
-  const std::vector<double>& box,
-  const std::vector<int>& type,
-  const std::vector<double>& position,
-  const NNAP_NL& nl,
-  std::vector<double>& ene_atom,
-  std::vector<double>& force,
-  std::vector<double>& virial_atom)
-{
-  // ================================
-  // Empty NNAP API interface
-  // NNAP developers only need to fill here:
-  //
-  // input:
-  //   nlocal              : number of local atoms
-  //   box[9]              : box matrix
-  //   type[nlocal]        : atom types
-  //   position[nlocal*3]  : xyzxyz...
-  //   nl                  : CPU neighbor list
-  //
-  // output format:
-  //   ene_atom[nlocal]
-  //   force[nlocal*3]       : fx fy fz ...
-  //   virial_atom[nlocal*9] : 3x3 per atom
-  //
-  // current template does nothing
-  // ================================
 }
 
 void NNAP::compute(
@@ -115,119 +43,12 @@ void NNAP::compute(
   GPU_Vector<double>& virial)
 {
   const int number_of_atoms = type.size();
-  const int grid_size = (number_of_atoms - 1) / BLOCK_SIZE_FORCE + 1;
-
-  // 1) build GPUMD neighbor list
-  nnap_data.NN.resize(number_of_atoms);
-  nnap_data.NL.resize(number_of_atoms * MAX_NEIGH_NUM_NNAP);
-  nnap_data.cell_count.resize(number_of_atoms);
-  nnap_data.cell_count_sum.resize(number_of_atoms);
-  nnap_data.cell_contents.resize(number_of_atoms);
-
-  find_neighbor(
-    N1,
-    N2,
-    rc,
-    box,
-    type,
-    position,
-    nnap_data.cell_count,
-    nnap_data.cell_count_sum,
-    nnap_data.cell_contents,
-    nnap_data.NN,
-    nnap_data.NL);
-
-  // 2) copy type
-  type_cpu.resize(number_of_atoms);
-  CHECK(gpuMemcpy(
-    type_cpu.data(),
-    type.data(),
-    sizeof(int) * number_of_atoms,
-    gpuMemcpyDeviceToHost));
-
-  // 3) copy position and transpose to xyzxyz...
-  position_gpu_trans.resize(number_of_atoms * 3);
-  position_transpose<<<grid_size, BLOCK_SIZE_FORCE>>>(
-    position.data(),
-    position_gpu_trans.data(),
-    number_of_atoms);
-  GPU_CHECK_KERNEL
-
-  position_cpu.resize(number_of_atoms * 3);
-  position_gpu_trans.copy_to_host(position_cpu.data());
-
-  // 4) copy box to CPU (3x3)
-  box_cpu.assign(9, 0.0);
-  box_cpu[0] = box.cpu_h[0];
-  box_cpu[1] = box.cpu_h[1];
-  box_cpu[2] = box.cpu_h[2];
-  box_cpu[3] = box.cpu_h[3];
-  box_cpu[4] = box.cpu_h[4];
-  box_cpu[5] = box.cpu_h[5];
-  box_cpu[6] = box.cpu_h[6];
-  box_cpu[7] = box.cpu_h[7];
-  box_cpu[8] = box.cpu_h[8];
-
-  // 5) copy current GPUMD outputs for future update
-  gpumd_pe_cpu.resize(number_of_atoms);
-  gpumd_force_cpu.resize(number_of_atoms * 3);
-  gpumd_virial_cpu.resize(number_of_atoms * 9);
-
-  potential.copy_to_host(gpumd_pe_cpu.data());
-  force.copy_to_host(gpumd_force_cpu.data());
-  virial.copy_to_host(gpumd_virial_cpu.data());
-
-  // 6) convert GPUMD neighbor list to CPU pointer-style list
-  nnap_nl.inum = number_of_atoms;
-  nnap_nl.ilist.resize(number_of_atoms);
-  nnap_nl.numneigh.resize(number_of_atoms);
-  nnap_nl.firstneigh.resize(number_of_atoms);
-
-  nnap_data.NN.copy_to_host(nnap_nl.numneigh.data());
-  cpu_NL.resize(nnap_data.NL.size());
-  nnap_data.NL.copy_to_host(cpu_NL.data());
-
-  int offset = 0;
-  nnap_nl.neigh_storage.resize(cpu_NL.size());
-  for (int i = 0; i < number_of_atoms; ++i) {
-    nnap_nl.ilist[i] = i;
-    nnap_nl.firstneigh[i] = nnap_nl.neigh_storage.data() + offset;
-    for (int j = 0; j < nnap_nl.numneigh[i]; ++j) {
-      nnap_nl.neigh_storage[offset + j] = cpu_NL[i + j * number_of_atoms];
-    }
-    offset += nnap_nl.numneigh[i];
-  }
-
-  // 7) reserve NNAP output buffers
-  nnap_ene_atom.assign(number_of_atoms, 0.0);
-  nnap_force.assign(number_of_atoms * 3, 0.0);
-  nnap_virial_atom.assign(number_of_atoms * 9, 0.0);
-
-  // 8) empty NNAP API call
-  nnap_api_compute(
-    number_of_atoms,
-    box_cpu,
-    type_cpu,
-    position_cpu,
-    nnap_nl,
-    nnap_ene_atom,
-    nnap_force,
-    nnap_virial_atom);
-
-  // 9) update GPUMD outputs
-  for (int i = 0; i < number_of_atoms; ++i) {
-    gpumd_pe_cpu[i] += nnap_ene_atom[i] * ener_unit_cvt_factor;
-  }
-  for (int i = 0; i < number_of_atoms * 3; ++i) {
-    gpumd_force_cpu[i] += nnap_force[i] * force_unit_cvt_factor;
-  }
-  for (int i = 0; i < number_of_atoms * 9; ++i) {
-    gpumd_virial_cpu[i] += nnap_virial_atom[i] * virial_unit_cvt_factor;
-  }
   
-  potential.copy_from_host(gpumd_pe_cpu.data());
-  force.copy_from_host(gpumd_force_cpu.data());
-  virial.copy_from_host(gpumd_virial_cpu.data());
+  // build neighbor list here
+  neighbor.find_neighbor_global(
+    rc,
+    box, 
+    type, 
+    position_per_atom);
   
 }
-#endif
--- src/force/nnap.cuh
+++ src/force/nnap.cuh
@@ -13,30 +13,14 @@
     along with GPUMD.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * NNAP interface for GPUMD
+ * @author liqa
+ */
+
 #pragma once
-#include "potential.cuh"
 #include "neighbor.cuh"
-#include <vector>
-#include <string>
-#include <stdio.h>
-
-#define MAX_NEIGH_NUM_NNAP 512
-
-struct NNAP_Data {
-  GPU_Vector<int> NN, NL;
-  GPU_Vector<int> cell_count;
-  GPU_Vector<int> cell_count_sum;
-  GPU_Vector<int> cell_contents;
-};
-
-// NNAP neighbor list in CPU format
-struct NNAP_NL {
-  int inum;
-  std::vector<int> ilist;
-  std::vector<int> numneigh;
-  std::vector<int*> firstneigh;
-  std::vector<int> neigh_storage;
-};
+#include "potential.cuh"
 
 class NNAP : public Potential
 {
@@ -52,46 +36,7 @@ public:
     GPU_Vector<double>& potential,
     GPU_Vector<double>& force,
     GPU_Vector<double>& virial);
-
-  void initialize_nnap(const char* filename);
-
+    
 protected:
-  double ener_unit_cvt_factor;
-  double dist_unit_cvt_factor;
-  double force_unit_cvt_factor;
-  double virial_unit_cvt_factor;
-
-  NNAP_Data nnap_data;
-  NNAP_NL nnap_nl;
-
-  GPU_Vector<double> position_gpu_trans;
-  std::vector<double> position_cpu;
-  std::vector<int> type_cpu;
-  std::vector<double> box_cpu;
-  std::vector<int> cpu_NL;
-
-  // cache current GPUMD outputs (for future update)
-  std::vector<double> gpumd_pe_cpu;
-  std::vector<double> gpumd_force_cpu;
-  std::vector<double> gpumd_virial_cpu;
-
-  // temporary output buffers for future NNAP use
-  std::vector<double> nnap_ene_atom;
-  std::vector<double> nnap_force;
-  std::vector<double> nnap_virial_atom;
-
-  GPU_Vector<double> e_f_v_gpu; // reserved for future transpose/update
-
-  void set_nnap_coeff();
-
-  // empty API interface: NNAP developers only need to fill this
-  void nnap_api_compute(
-    int nlocal,
-    const std::vector<double>& box,
-    const std::vector<int>& type,
-    const std::vector<double>& position,
-    const NNAP_NL& nl,
-    std::vector<double>& ene_atom,
-    std::vector<double>& force,
-    std::vector<double>& virial_atom);
+  Neighbor neighbor;
 };
